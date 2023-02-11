aec8021b48f54614bf161956e38544fd14336098
gomin0 <80516484+gomin0@users.noreply.github.com>
2023-02-11 17:43:30 +0900

    [Silver IV] Title: 제로, Time: 228 ms, Memory: 23308 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Stack<Integer> stack = new Stack<Integer>();
+
+        int K = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < K; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if(num == 0) {
+                stack.pop();
+            } else {
+                stack.push(num);
+            }
+        }
+        int sum = 0;
+
+        for(int i : stack) {
+            sum += i;
+        }
+
+        System.out.println(sum);
+    }
+}
