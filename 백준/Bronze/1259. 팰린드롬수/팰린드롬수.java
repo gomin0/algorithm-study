997f4d79f52c393764c57693c4d96f85859824c6
gomin0 <80516484+gomin0@users.noreply.github.com>
2023-02-19 13:37:03 +0900

    [Bronze I] Title: 팰린드롬수, Time: 116 ms, Memory: 14076 KB -BaekjoonHub

---
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        while(true) {
+            String str = br.readLine();
+
+            if(str.equals("0")) {
+                break;
+            }
+
+            String reverse = new StringBuilder(str).reverse().toString();
+
+            if(str.equals(reverse)) {
+                sb.append("yes").append('\n');
+            }else {
+                sb.append("no").append('\n');
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
