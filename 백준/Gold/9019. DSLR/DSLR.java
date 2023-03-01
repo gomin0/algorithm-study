161b965a4a35f1889d416719cd421cae2e9d769d
gomin0 <80516484+gomin0@users.noreply.github.com>
2023-03-01 18:04:22 +0900

    [Gold IV] Title: DSLR, Time: 3388 ms, Memory: 315160 KB -BaekjoonHub

---
+++ 백준/Gold/9019. DSLR/DSLR.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        while (T-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            Queue<Integer> q = new LinkedList<>();
+            boolean[] visited = new boolean[10000]; //방문 여부
+            String[] command = new String[10000]; //정답 명령어 담는 배열
+
+            q.add(A);
+            visited[A] = true;
+            Arrays.fill(command, ""); //배열 초기화
+
+            while (!q.isEmpty() && !visited[B]) {
+                int now = q.poll(); //현재 탐색 위치
+
+                int D = (2 * now) % 10000;
+                int S = now == 0 ? 9999 : now - 1;
+                int L = (now % 1000) * 10 + now / 1000;
+                int R = (now % 10) * 1000 + now / 10;
+
+                if (!visited[D]) {
+                    q.add(D);
+                    visited[D] = true;
+                    command[D] = command[now] + "D";
+                }
+
+                if (!visited[S]) {
+                    q.add(S);
+                    visited[S] = true;
+                    command[S] = command[now] + "S";
+                }
+
+                if (!visited[L]) {
+                    q.add(L);
+                    visited[L] = true;
+                    command[L] = command[now] + "L";
+                }
+
+                if (!visited[R]) {
+                    q.add(R);
+                    visited[R] = true;
+                    command[R] = command[now] + "R";
+                }
+            }
+            System.out.println(command[B]);
+        }
+    }
+}
