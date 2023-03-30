9018c5c248f54df75f666ccd43ab8e6920e4c0a7
gomin0 <gomin0981113@naver.com>
2023-03-30 13:52:00 +0900

    [Silver I] Title: 단지번호붙이기, Time: 140 ms, Memory: 14344 KB -BaekjoonHub

---
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+
+    static int[][] map;
+    static int[] dx = {-1, 0, 1, 0}; // 상, 우, 하, 좌
+    static int[] dy = {0, 1, 0, -1};
+    static int n, cnt;
+    static ArrayList<Integer> list = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        map = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < n; j++) {
+                map[i][j] = s.charAt(j) - '0';
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (map[i][j] == 1) {
+                    cnt = 0;
+                    dfs(i, j);
+                    list.add(cnt);
+                }
+            }
+        }
+
+        Collections.sort(list);
+        System.out.println(list.size());
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+    }
+
+    static void dfs(int x, int y) {
+        map[x][y] = 0; // 방문 처리
+        cnt++;
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue; // 범위를 벗어나는 경우
+            if (map[nx][ny] != 1) continue; // 방문할 수 없는 경우
+
+            dfs(nx, ny); // 다음 노드 탐색
+        }
+    }
+
+}
