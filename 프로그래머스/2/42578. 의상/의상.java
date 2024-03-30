5a04bc534c41c1ec2272ec0ce082b52c1dc93b54
gomin0 <gomin0981113@naver.com>
2024-03-30 19:38:14 +0900

    [level 2] Title: 의상, Time: 0.05 ms, Memory: 72 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    public int solution(String[][] clothes) {
+        Map<String, Integer> map = new HashMap<>();
+        int answer = 1;
+        for (int i = 0; i < clothes.length; i++) {
+            int count = 1;
+            if (map.get(clothes[i][1]) == null) {
+                map.put(clothes[i][1], 1);
+            }
+            else {
+                map.put(clothes[i][1], map.get(clothes[i][1]) + 1);
+            }
+        }
+        
+        for(String s : map.keySet()) {
+			answer *= (map.get(s) + 1);
+		}
+        
+        return answer - 1;
+    }
+}
