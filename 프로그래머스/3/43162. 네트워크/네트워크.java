06fd0d5ed3544da178c51338f28c52182338d19a
gomin0 <gomin0981113@naver.com>
2024-04-10 21:22:48 +0900

    [level 3] Title: 네트워크, Time: 0.34 ms, Memory: 84 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,24 +1,58 @@
+//bfs
+import java.util.*;
 class Solution {
-    static boolean visited[];    
+    static boolean visited[];
     public int solution(int n, int[][] computers) {
         int answer = 0;
         visited = new boolean[n];
         
         for (int i = 0; i < n; i++) {
-            if (visited[i] == false) {
+            if(visited[i] == false) {
+                bfs(i, computers, n);
                 answer++;
-                dfs(i, computers, n);
             }
         }
         return answer;
     }
-    
-    static void dfs(int i, int[][] computers, int n) {
+    static void bfs(int i, int[][] computers, int n) {
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(i);
         visited[i] = true;
-        for (int j = 0; j < n; j++) {
-            if (visited[j] == false && computers[i][j] == 1) {
-                dfs(j, computers, n);
+        
+        while (!q.isEmpty()) {
+            int a = q.poll();
+            for (int j = 0; j < n; j++) {
+                if (visited[j] == false && computers[a][j] == 1) {
+                    visited[j] = true;
+                    q.offer(j);
+                }
             }
         }
     }
 }
+
+//dfs
+// class Solution {
+//     static boolean visited[];    
+//     public int solution(int n, int[][] computers) {
+//         int answer = 0;
+//         visited = new boolean[n];
+        
+//         for (int i = 0; i < n; i++) {
+//             if (visited[i] == false) {
+//                 answer++;
+//                 dfs(i, computers, n);
+//             }
+//         }
+//         return answer;
+//     }
+    
+//     static void dfs(int i, int[][] computers, int n) {
+//         visited[i] = true;
+//         for (int j = 0; j < n; j++) {
+//             if (visited[j] == false && computers[i][j] == 1) {
+//                 dfs(j, computers, n);
+//             }
+//         }
+//     }
+// }
