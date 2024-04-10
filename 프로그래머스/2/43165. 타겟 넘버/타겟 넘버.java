ee42686525b6fe3c00d3ea013d8ae112aa5e2109
gomin0 <gomin0981113@naver.com>
2024-04-10 16:12:23 +0900

    [level 2] Title: 타겟 넘버, Time: 0.46 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,23 @@
+class Solution {
+    static boolean[] visited;
+    static int answer = 0;
+    public int solution(int[] numbers, int target) {
+        dfs(0, numbers, target, 0);
+        return answer;
+    }
+    
+    static void dfs(int depth, int[] numbers, int target, int result) {
+        if (depth == numbers.length) {
+            if (target == result) {
+                answer++;
+            }
+            return;
+        }
+        
+        int plus = result + numbers[depth];
+        int minus = result - numbers[depth];
+        
+        dfs (depth + 1, numbers, target, plus);
+        dfs (depth + 1, numbers, target, minus);
+    }
+}
