01df6c0b4aa03224bc7bfe10d5145912d15f653d
gomin0 <gomin0981113@naver.com>
2024-07-07 11:08:23 +0900

    [level 1] Title: 카드 뭉치, Time: 0.00 ms, Memory: 9.98 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -0,0 +1,11 @@
+def solution(cards1, cards2, goal):
+    
+    for i in goal:
+        if len(cards1) != 0 and i == cards1[0]:
+            cards1.remove(i)
+        elif len(cards2) != 0 and i == cards2[0]:
+            cards2.remove(i)
+        else:
+            return "No"
+            
+    return "Yes"
