b77013991599cce0a1be58d34bfa4a963cafaf27
gomin0 <gomin0981113@naver.com>
2024-07-10 13:43:00 +0900

    [level 1] Title: 최소직사각형, Time: 6.05 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/README.md
+++ 프로그래머스/1/86491. 최소직사각형/README.md
@@ -1,10 +1,10 @@
 # [level 1] 최소직사각형 - 86491 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86491?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86491) 
 
 ### 성능 요약
 
-메모리: 11.5 MB, 시간: 2.95 ms
+메모리: 11.6 MB, 시간: 6.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 29일 14:21:39
+2024년 07월 10일 13:42:57
 
 ### 문제 설명
 
--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -5,12 +5,8 @@ def solution(sizes):
     column = []
     
     for i in range(len(sizes)):
-        if sizes[i][0] < sizes[i][1]:
-            row.append(sizes[i][1])
-            column.append(sizes[i][0])
-        else:
-            row.append(sizes[i][0])
-            column.append(sizes[i][1])
+        row.append(max(sizes[i][0], sizes[i][1]))
+        column.append(min(sizes[i][0], sizes[i][1]))
     
     answer = max(row) * max(column)
     
