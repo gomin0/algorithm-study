3cde8602763a4553d026dd26cfd115f8022f8d18
gomin0 <gomin0981113@naver.com>
2024-07-14 11:08:48 +0900

    [level 1] Title: 옹알이 (2), Time: 0.05 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
@@ -1,21 +1,16 @@
 def solution(babbling):
-    
+
     answer = 0
     speek = ["aya", "ye", "woo", "ma"]
 
     for word in babbling:
         speeking = 0
-        prev_sound = ""
         valid = True
         
         while speeking < len(word):
             matched = False
             for sound in speek:
-                if word.startswith(sound, speeking):
-                    if sound == prev_sound:
-                        valid = False
-                        break
-                    prev_sound = sound
+                if word.startswith(sound, speeking) and not word.startswith(sound * 2, speeking):
                     speeking += len(sound)
                     matched = True
                     break
