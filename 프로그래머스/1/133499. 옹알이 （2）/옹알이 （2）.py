b03a92d882fbbb5816ca0b917f90d76860968b0d
gomin0 <gomin0981113@naver.com>
2024-07-15 09:54:46 +0900

    Update 옹알이 （2）.py

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
@@ -2,23 +2,28 @@ def solution(babbling):
 
     answer = 0
     speek = ["aya", "ye", "woo", "ma"]
-
-    for word in babbling:
-        speeking = 0
-        valid = True
-        
-        while speeking < len(word):
-            matched = False
-            for sound in speek:
-                if word.startswith(sound, speeking) and not word.startswith(sound * 2, speeking):
-                    speeking += len(sound)
-                    matched = True
-                    break
-            if not matched:
-                valid = False
-                break
-        
-        if valid:
-            answer += 1
     
+    for b in babbling:
+        if "ayaaya" in b or "yeye" in b or "woowoo" in b or "mama" in b:
+            continue    
+        if not b.replace("aya", " ").replace("ye", " ").replace("woo", " ").replace("ma", " ").replace(" ", ""):
+            answer += 1
     return answer
+
+#     for word in babbling:
+#         speeking = 0
+#         valid = True
+        
+#         while speeking < len(word):
+#             matched = False
+#             for sound in speek:
+#                 if word.startswith(sound, speeking) and not word.startswith(sound * 2, speeking):
+#                     speeking += len(sound)
+#                     matched = True
+#                     break
+#             if not matched:
+#                 valid = False
+#                 break
+        
+#         if valid:
+#             answer += 1
