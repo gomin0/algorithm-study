e2cb8788b45df560b5c84bf4c2f8b15e64cb4d8b
gomin0 <gomin0981113@naver.com>
2024-07-22 13:27:49 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.34 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
@@ -1,24 +1,24 @@
-def solution(wallpaper):  
-    min_x = 50
-    min_y = 50
-    max_x = 0
-    max_y = 0
+# def solution(wallpaper):  
+#     min_x = 50
+#     min_y = 50
+#     max_x = 0
+#     max_y = 0
     
-    for i in range(len(wallpaper)):
-        for j in range(len(wallpaper[0])):
-            if wallpaper[i][j] == "#":
-                min_x = min(min_x, i)
-                min_y = min(min_y, j)
-                max_x = max(max_x, i + 1)
-                max_y = max(max_y, j + 1)
+#     for i in range(len(wallpaper)):
+#         for j in range(len(wallpaper[0])):
+#             if wallpaper[i][j] == "#":
+#                 min_x = min(min_x, i)
+#                 min_y = min(min_y, j)
+#                 max_x = max(max_x, i + 1)
+#                 max_y = max(max_y, j + 1)
                 
-    return [min_x, min_y, max_x, max_y]
+#     return [min_x, min_y, max_x, max_y]
 
-# def solution(wall):
-#     a, b = [], []
-#     for i in range(len(wall)):
-#         for j in range(len(wall[i])):
-#             if wall[i][j] == "#":
-#                 a.append(i)
-#                 b.append(j)
-#     return [min(a), min(b), max(a) + 1, max(b) + 1]
+def solution(wall):
+    a, b = [], []
+    for i in range(len(wall)):
+        for j in range(len(wall[i])):
+            if wall[i][j] == "#":
+                a.append(i)
+                b.append(j)
+    return [min(a), min(b), max(a) + 1, max(b) + 1]
