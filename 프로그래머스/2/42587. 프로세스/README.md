a56ffe524b79e3ed3f6fa11a73f180035b1d0844
gomin0 <gomin0981113@naver.com>
2024-09-20 21:07:28 +0900

    [level 2] Title: 프로세스, Time: 1.91 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/README.md
+++ 프로그래머스/2/42587. 프로세스/README.md
@@ -1,10 +1,10 @@
 # [level 2] 프로세스 - 42587 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587?language=java) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 1.25 ms
+메모리: 77.5 MB, 시간: 1.91 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 02일 21:41:22
+2024년 09월 20일 21:07:24
 
 ### 문제 설명
 
--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,40 +1,77 @@
 import java.util.*;
 class Solution {
     public int solution(int[] priorities, int location) {
-        Queue<Integer> q = new LinkedList<>();
         int answer = 0;
-        // int find = priorities[location];
-        for (int proccess : priorities) {
-            q.add(proccess);
+        Queue<Integer> q = new LinkedList<>();
+        for (int process : priorities) {
+            q.add(process);
         }
         
         while(!q.isEmpty()) {
             int a = q.poll();
-            boolean check = false;
-            for(int i : q) {
-                if(i > a) {
-                    check = true;
+            boolean big = true;
+            for (int i : q) {
+                if (i > a) {
+                    big = false;
                     break;
                 }
             }
-            if (check) {
+            if (big == true) {
+                answer++;
+                if (location == 0) {
+                    return answer;
+                }
+            } else {
                 q.add(a);
             }
-            else{
-                answer++;
-                // if(a == find) {
-                // break;
-                // } 
-                if (location == 0) {
-                    break;
-                }
-            }
-            location --;
-            if (location < 0) {
+            
+            if (location == 0) {
                 location = q.size() - 1;
+            } else {
+                location--;
             }
         }
-        
         return answer;
     }
 }
+
+// import java.util.*;
+// class Solution {
+//     public int solution(int[] priorities, int location) {
+//         Queue<Integer> q = new LinkedList<>();
+//         int answer = 0;
+//         // int find = priorities[location];
+//         for (int proccess : priorities) {
+//             q.add(proccess);
+//         }
+        
+//         while(!q.isEmpty()) {
+//             int a = q.poll();
+//             boolean check = false;
+//             for(int i : q) {
+//                 if(i > a) {
+//                     check = true;
+//                     break;
+//                 }
+//             }
+//             if (check) {
+//                 q.add(a);
+//             }
+//             else{
+//                 answer++;
+//                 // if(a == find) {
+//                 // break;
+//                 // } 
+//                 if (location == 0) {
+//                     break;
+//                 }
+//             }
+//             location --;
+//             if (location < 0) {
+//                 location = q.size() - 1;
+//             }
+//         }
+        
+//         return answer;
+//     }
+// }
