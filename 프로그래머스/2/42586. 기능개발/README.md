39bed213357a27f074768ab6c225f46399d279b1
gomin0 <gomin0981113@naver.com>
2024-09-20 20:12:38 +0900

    [level 2] Title: 기능개발, Time: 0.05 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=java) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.04 ms
+메모리: 72.6 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 02일 11:09:45
+2024년 09월 20일 20:12:35
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,47 +1,78 @@
 import java.util.*;
+
 class Solution {
-    public int[] solution(int[] progresses, int[] speeds) {
-        ArrayList<Integer> list = new ArrayList<>();
-        int[] finish = new int[progresses.length];
-        for (int i = 0; i < progresses.length; i++) {
-            if ((100 - progresses[i]) % speeds[i] == 0) {
-                finish[i] = (100 - progresses[i]) / speeds[i];
-            }
-            else {
-                finish[i] = (100 - progresses[i]) / speeds[i] + 1;
-            }
-        }
+    public ArrayList<Integer> solution(int[] progresses, int[] speeds) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        int[] done = new int[progresses.length];
+        int day = 0;
+        int count = 0;
         
-        // for (int i = 0; i < finish.length; i++) {
-        //     int count = 0;
-        //     for (int j = i + 1; j < finish.length; j++) {
-        //         count++;
-        //         if (finish[i] < finish[j]) {
-        //             list.add(count);
-        //             break;
-        //         }
-        //     }
-        // }
-        
-        int x = finish[0];
-        int count = 1;
-        for(int i = 1; i < finish.length; i++) {
-            if (x >= finish[i]) {
-                count++;
+        for(int i = 0; i < progresses.length; i++) {
+            int nextDay = (100 - progresses[i]) / speeds[i];
+            if ((100 - progresses[i]) % speeds[i] != 0) {
+                nextDay += 1;
             }
-            else {
-                list.add(count);
+            
+            if (day >= nextDay) {
+                count += 1;
+            } else{
+                if (count != 0) {
+                    answer.add(count);
+                }
                 count = 1;
-                x = finish[i];
+                day = nextDay;
             }
         }
-        list.add(count);
-        
-        int[] answer = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            answer[i] = list.get(i);
-        }
+        answer.add(count);
         
         return answer;
     }
 }
+
+// import java.util.*;
+// class Solution {
+//     public int[] solution(int[] progresses, int[] speeds) {
+//         ArrayList<Integer> list = new ArrayList<>();
+//         int[] finish = new int[progresses.length];
+//         for (int i = 0; i < progresses.length; i++) {
+//             if ((100 - progresses[i]) % speeds[i] == 0) {
+//                 finish[i] = (100 - progresses[i]) / speeds[i];
+//             }
+//             else {
+//                 finish[i] = (100 - progresses[i]) / speeds[i] + 1;
+//             }
+//         }
+        
+//         // for (int i = 0; i < finish.length; i++) {
+//         //     int count = 0;
+//         //     for (int j = i + 1; j < finish.length; j++) {
+//         //         count++;
+//         //         if (finish[i] < finish[j]) {
+//         //             list.add(count);
+//         //             break;
+//         //         }
+//         //     }
+//         // }
+        
+//         int x = finish[0];
+//         int count = 1;
+//         for(int i = 1; i < finish.length; i++) {
+//             if (x >= finish[i]) {
+//                 count++;
+//             }
+//             else {
+//                 list.add(count);
+//                 count = 1;
+//                 x = finish[i];
+//             }
+//         }
+//         list.add(count);
+        
+//         int[] answer = new int[list.size()];
+//         for (int i = 0; i < list.size(); i++) {
+//             answer[i] = list.get(i);
+//         }
+        
+//         return answer;
+//     }
+// }
