1347e21fb652af10d15b2dcfb642d285a10c709f
gomin0 <gomin0981113@naver.com>
2024-09-20 19:38:36 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 18.02 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/README.md
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/README.md
@@ -1,10 +1,10 @@
 # [level 1] 같은 숫자는 싫어 - 12906 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906?language=java) 
 
 ### 성능 요약
 
-메모리: 27.8 MB, 시간: 53.86 ms
+메모리: 111 MB, 시간: 18.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 27일 21:59:43
+2024년 09월 20일 19:38:30
 
 ### 문제 설명
 
--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -1,19 +1,37 @@
 import java.util.*;
 
 public class Solution {
-    public int[] solution(int []arr) {
-        List<Integer> answerList = new ArrayList<>();
+    public ArrayList<Integer> solution(int []arr) {
+        int[] answer = {};
+        ArrayList<Integer> answerList = new ArrayList<>();
         answerList.add(arr[0]);
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] != arr[i - 1]) {
+        
+        for(int i = 1; i < arr.length; i++) {
+            if(arr[i] != arr[i-1]) {
                 answerList.add(arr[i]);
             }
         }
-        int[] answer = new int[answerList.size()];
-        for (int i = 0; i < answerList.size(); i++) {
-            answer[i] = answerList.get(i);
-        }
-        
-        return answer;
+
+        return answerList;
     }
 }
+
+// import java.util.*;
+
+// public class Solution {
+//     public int[] solution(int []arr) {
+//         List<Integer> answerList = new ArrayList<>();
+//         answerList.add(arr[0]);
+//         for (int i = 1; i < arr.length; i++) {
+//             if (arr[i] != arr[i - 1]) {
+//                 answerList.add(arr[i]);
+//             }
+//         }
+//         int[] answer = new int[answerList.size()];
+//         for (int i = 0; i < answerList.size(); i++) {
+//             answer[i] = answerList.get(i);
+//         }
+        
+//         return answer;
+//     }
+// }
