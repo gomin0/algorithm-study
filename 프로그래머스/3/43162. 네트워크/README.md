25f4e66054108d2779a6a63a2ed169adbfbe1021
gomin0 <gomin0981113@naver.com>
2024-10-04 21:01:04 +0900

    [level 3] Title: 네트워크, Time: 0.53 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.87 ms
+메모리: 10.3 MB, 시간: 0.53 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 04일 20:52:43
+2024년 10월 04일 21:01:00
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,24 +1,42 @@
-from collections import deque
-
+#dfs
 def solution(n, computers):
-    def bfs(start):
-        queue = deque([start])
-        visited[start] = True
-        
-        while queue:
-            node = queue.popleft()
-            
-            for i in range(n):
-                if computers[node][i] == 1 and not visited[i]:
-                    queue.append(i)
-                    visited[i] = True
-                    
+    def dfs(node):
+        visited[node] = True
+        for i in range(n):
+            if computers[node][i] == 1 and not visited[i]:
+                dfs(i)
+                
     visited = [False for _ in range(n)]
     
     count = 0
     for i in range(n):
         if not visited[i]:
-            bfs(i)
+            dfs(i)
             count += 1
-            
     return count
+
+# bfs
+# from collections import deque
+
+# def solution(n, computers):
+#     def bfs(start):
+#         queue = deque([start])
+#         visited[start] = True
+        
+#         while queue:
+#             node = queue.popleft()
+            
+#             for i in range(n):
+#                 if computers[node][i] == 1 and not visited[i]:
+#                     queue.append(i)
+#                     visited[i] = True
+                    
+#     visited = [False for _ in range(n)]
+    
+#     count = 0
+#     for i in range(n):
+#         if not visited[i]:
+#             bfs(i)
+#             count += 1
+            
+#     return count
