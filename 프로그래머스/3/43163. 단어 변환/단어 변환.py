a68fab99bdf6a27f4e72746b023990ee96ba2cc9
gomin0 <gomin0981113@naver.com>
2024-10-06 11:49:49 +0900

    [level 3] Title: 단어 변환, Time: 0.34 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -0,0 +1,32 @@
+from collections import deque
+
+def word_diff(word1, word2):
+    diff_count = 0
+    for w1, w2 in zip(word1, word2):
+        if w1 != w2:
+            diff_count += 1
+        if diff_count > 1:
+            return False
+    return diff_count == 1
+
+def solution(begin, target, words):
+    answer = 0
+    
+    if target not in words:
+        return 0
+    
+    queue = deque([(begin, 0)])  # 단어, 변환 수
+    visited = set([begin])
+    
+    while queue:
+        word, count = queue.popleft()
+        
+        if word == target:
+            return count
+        
+        for w in words:
+            if w not in visited and word_diff(word, w):
+                visited.add(w)
+                queue.append((w, count + 1))
+
+    return 0
