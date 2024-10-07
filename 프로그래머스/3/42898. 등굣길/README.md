be01f4471b15c700ca30e3343a882d67370ab0bf
gomin0 <gomin0981113@naver.com>
2024-10-07 14:56:09 +0900

    [level 3] Title: 등굣길, Time: 3.41 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/README.md
+++ 프로그래머스/3/42898. 등굣길/README.md
@@ -1,10 +1,10 @@
 # [level 3] 등굣길 - 42898 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42898) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42898?language=python3) 
 
 ### 성능 요약
 
-메모리: 51.8 MB, 시간: 0.64 ms
+메모리: 10.3 MB, 시간: 3.41 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 10일 23:46:46
+2024년 10월 07일 14:56:05
 
 ### 문제 설명
 
---
+++ 프로그래머스/3/42898. 등굣길/등굣길.py
@@ -0,0 +1,25 @@
+def solution(m, n, puddles):
+    answer = 0
+    
+    dp = [[0] * m for _ in range(n)]
+    
+    dp[0][0] = 1
+    
+    for puddle in puddles:
+        x, y = puddle
+        dp[y-1][x-1] = -1
+        
+    for i in range(n):
+        for j in range(m):
+            if dp[i][j] == -1:
+                dp[i][j] = 0
+                continue
+            
+            if i > 0:
+                dp[i][j] += dp[i-1][j]
+            if j > 0:
+                dp[i][j] += dp[i][j-1]
+                
+            dp[i][j] %= 1_000_000_007
+    
+    return dp[n-1][m-1]
