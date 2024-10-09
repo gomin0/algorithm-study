192f525267d75a17c0d829fbb69b2e67c361a391
gomin0 <gomin0981113@naver.com>
2024-10-09 15:06:19 +0900

    [level 3] Title: 합승 택시 요금, Time: 21.79 ms, Memory: 15.5 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
@@ -1,38 +1,40 @@
 import heapq
+from collections import defaultdict
 
 def dijkstra(start, n, graph):
     distance = {i: float('inf') for i in range(1, n+1)}
     distance[start] = 0
     
     queue = []
-    heapq.heappush(queue, (start, 0))
+    heapq.heappush(queue, (0, start))
     
     while queue:
-        now_node, now_distance = heapq.heappop(queue)
+        now_distance, now_node = heapq.heappop(queue)
         if now_distance > distance[now_node]:
             continue
-        for next_node, next_distance in graph[now_node]:
+        
+        for next_distance, next_node in graph[now_node]:
             dist = now_distance + next_distance
             if dist < distance[next_node]:
                 distance[next_node] = dist
-                heapq.heappush(queue, (next_node, dist))
+                heapq.heappush(queue, (dist, next_node))
     return distance
 
 def solution(n, s, a, b, fares):
     answer = float('inf')
-    graph = [[] for i in range(n+1)]
     
+    graph = defaultdict(list)
     for start, end, d in fares:
-        graph[start].append((end, d))
-        graph[end].append((start, d))
-    
-    ab_distance = dijkstra(s, n, graph)
-    a_distance = dijkstra(a, n, graph)
-    b_distance = dijkstra(b, n, graph)
+        graph[start].append((d, end))
+        graph[end].append((d, start))
+        
+    ab_taxi = dijkstra(s, n, graph)
+    a_taxi = dijkstra(a, n, graph)
+    b_taxi = dijkstra(b, n, graph)
     
     for i in range(1, n+1):
-        total_distance = ab_distance[i] + a_distance[i] + b_distance[i]
-        if total_distance < answer:
-            answer = total_distance
+        total_taxi = ab_taxi[i] + a_taxi[i] + b_taxi[i]
+        if answer > total_taxi:
+            answer = total_taxi
     
     return answer
