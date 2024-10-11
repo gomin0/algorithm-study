2c6fb6919388387f99ed158973baa74b7aac64fe
gomin0 <gomin0981113@naver.com>
2024-10-11 13:30:12 +0900

    [level 2] Title: 배달, Time: 52.49 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.py
+++ 프로그래머스/2/12978. 배달/배달.py
@@ -1,3 +1,33 @@
+# 플로이드 워셜
+def floyd_warshall(graph, N):
+    for k in range(1, N+1):
+        for i in range(1, N+1):
+            for j in range(1, N+1):
+                graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+    return graph
+    
+
+def solution(N, road, K):
+    answer = 0
+
+    graph = [[float('inf')] * (N+1) for _ in range(N+1)]
+    
+    for i in range(1, N+1):
+        graph[i][i] = 0
+        
+    for start, end, time in road:
+        graph[start][end] = min(graph[start][end], time)
+        graph[end][start] = min(graph[end][start], time)
+    
+    graph = floyd_warshall(graph, N)
+    
+    for j in range(1, N + 1):
+        if graph[1][j] <= K:
+            answer += 1
+
+    return answer
+
+
 # 다익스트라
 import heapq
 from collections import defaultdict
@@ -23,7 +53,7 @@ def dijkstra(start, N, graph):
     
     return distance
 
-def solution(N, road, K):
+def solution2(N, road, K):
     answer = 0
 
     graph = defaultdict(list)
