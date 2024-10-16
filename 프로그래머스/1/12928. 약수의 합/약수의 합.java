2451f6659cbbf186bb6fb0c3d6fa9a0dc03997f4
gomin0 <gomin0981113@naver.com>
2024-10-16 14:44:57 +0900

    [level 1] Title: 약수의 합, Time: 0.09 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -1,10 +1,14 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
+        int num = (int) Math.sqrt(n);
         
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i <= num; i++) {
             if (n % i == 0) {
                 answer += i;
+                if (i != n/i){
+                    answer += n / i;
+                }
             }
         }
         
