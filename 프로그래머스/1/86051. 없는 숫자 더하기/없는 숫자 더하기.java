472e5504a2828e1fd315de90cb7249967216dee6
gomin0 <gomin0981113@naver.com>
2024-10-16 19:43:47 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 80.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        
+        for (int i = 0; i <= 9; i++) {
+            answer += i;
+        }
+        
+        for (int j = 0; j < numbers.length; j++) {
+            answer -= numbers[j];
+        }
+        
+        return answer;
+    }
+}
