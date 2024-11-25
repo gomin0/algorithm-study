0e52f82720a7461ed3f626b340e84ebe9b1bae8d
gomin0 <gomin0981113@naver.com>
2024-11-25 10:13:40 +0900

    [level 1] Title: 콜라 문제, Time: 0.08 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int a, int b, int n) {
+        int answer = 0;
+        
+        while (n >= a) {
+            int bottle = (n / a) * b;
+            int rest = n % a;
+            answer += bottle;
+            n = rest + bottle;
+        }
+        
+        return answer;
+    }
+}
