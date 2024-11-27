f87ca3d2052d62c93c30cce5effc0f76d9c857ff
gomin0 <gomin0981113@naver.com>
2024-11-27 21:11:27 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 4.42 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/README.md
+++ 프로그래머스/3/150365. 미로 탈출 명령어/README.md
@@ -1,10 +1,10 @@
 # [level 3] 미로 탈출 명령어 - 150365 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150365?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150365) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 5.41 ms
+메모리: 10.2 MB, 시간: 4.42 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 27일 21:01:02
+2024년 11월 27일 21:11:24
 
 ### 문제 설명
 
--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.py
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.py
@@ -1,32 +1,39 @@
 from collections import deque
 
 def solution(n, m, x, y, r, c, k):
-    answer = ''
-    # 남은 거리 탐색 자주 해주어야 하므로 함수로 빼주기
-    def manhattan(x1, y1):
-        return abs(x1 - (r-1)) + abs(y1-(c-1))
+    # 방향 정의 및 사전순 탐색 보장
+    direct = {(1, 0): 'd', (0, -1): 'l', (0, 1): 'r', (-1, 0): 'u'}
 
-    # k가 최단 거리보다 작거나, 최단 거리 - k가 홀수라면 도착지에 k번만에 도착 불가
-    if manhattan(x-1, y-1) > k or (manhattan(x-1, y-1) - k) % 2:
-        return 'impossible'
-    # 탐색 방향 사전순으로 - d l r u
-    direct = {(1,0):'d', (0,-1):'l', (0,1):'r', (-1,0):'u'}
-    q = deque()
-    q.append((x-1, y-1, 0, ''))
-    while q:
-        si, sj, cnt, route = q.popleft()
-        # 도착했는데 남은 거리가 홀수라면 도착지에 k만큼 오지 못한다!
-        if (si, sj) == (r-1, c-1) and (k-cnt) % 2:
-            return 'impossible'
-        elif (si, sj) == (r-1, c-1) and cnt == k:
-            return route
+    # 목표까지의 맨해튼 거리 계산
+    def calculate_distance(x1, y1):
+        return abs(x1 - (r - 1)) + abs(y1 - (c - 1))
+
+    # 목표까지 도달 가능한지 사전 검사
+    initial_distance = calculate_distance(x - 1, y - 1)
+    if initial_distance > k or (k - initial_distance) % 2 != 0:
+        return "impossible"
+
+    # BFS 탐색 시작
+    queue = deque([(x - 1, y - 1, 0, "")])  # (현재 x, 현재 y, 이동 횟수, 경로)
+
+    while queue:
+        si, sj, cnt, route = queue.popleft()
+
+        # 목표 지점 도달 확인
+        if (si, sj) == (r - 1, c - 1):
+            if cnt == k:
+                return route
+            if (k - cnt) % 2:  # 남은 거리가 홀수면 도달 불가능
+                return "impossible"
+
+        # 사전순으로 네 방향 탐색
         for di, dj in direct:
-            ni, nj = si+di, sj+dj
-            if 0<=ni<n and 0<=nj<m:
-                # 다음 이동 자리를 보는 것이므로 +1 을 해주어야 함
-                if manhattan(ni, nj) + cnt + 1 > k:
-                    continue
-                q.append((ni, nj, cnt+1, route+direct[(di, dj)]))
-                break
+            ni, nj = si + di, sj + dj
+            if 0 <= ni < n and 0 <= nj < m:
+                # 목표까지 도달 가능한지 확인 후 큐에 추가
+                next_distance = calculate_distance(ni, nj)
+                if next_distance + cnt + 1 <= k:
+                    queue.append((ni, nj, cnt + 1, route + direct[(di, dj)]))
+                    break  # 한 방향만 추가 (큐 크기 제한)
 
-    return answer
+    return "impossible"
