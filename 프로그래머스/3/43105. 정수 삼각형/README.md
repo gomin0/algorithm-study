672dda68ba917727119b07049f6440b52ff20efc
gomin0 <gomin0981113@naver.com>
2024-12-01 12:31:44 +0900

    [level 3] Title: 정수 삼각형, Time: 56.70 ms, Memory: 19.1 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/README.md
+++ 프로그래머스/3/43105. 정수 삼각형/README.md
@@ -1,10 +1,10 @@
 # [level 3] 정수 삼각형 - 43105 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43105?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43105) 
 
 ### 성능 요약
 
-메모리: 19.1 MB, 시간: 49.25 ms
+메모리: 19.1 MB, 시간: 56.70 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 04일 10:51:44
+2024년 12월 01일 12:31:39
 
 ### 문제 설명
 
--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
@@ -1,30 +1,16 @@
 def solution(triangle):
     answer = 0
-    
     n = len(triangle)
-    dp = [[0] * (d+1) for d in range(n)]
-
+    dp = [[0] * (i+1) for i in range(n)]
     dp[0][0] = triangle[0][0]
     
     for i in range(1, n):
-        for j in range(i+1):
-            tri = triangle[i][j]
+        for j in range(len(triangle[i])):
             if j == 0:
-                dp[i][j] = tri + dp[i-1][j]
-            elif j == i:
-                dp[i][j] = tri + dp[i-1][j-1]
+                dp[i][j] = dp[i-1][j] + triangle[i][j]
+            elif j == len(triangle[i]) - 1:
+                dp[i][j] = dp[i-1][j-1] + triangle[i][j]
             else:
-                dp[i][j] = tri + max(dp[i-1][j-1], dp[i-1][j])
-        
+                dp[i][j] = max(dp[i-1][j-1], dp[i-1][j]) + triangle[i][j]
+    
     return max(dp[n-1])
-
-# dp[4][0] = triangle[4][0] + dp[3][0]
-# dp[4][1] = triangle[4][1] + max(dp[3][0], dp[3][1])
-# ...
-# dp[4][4] = triangle[4][4] + dp[3][3]
-
-# [7]
-# [3, 8]
-# [8, 1, 0]
-# [2, 7, 4, 4]
-# [4, 5, 2, 6, 5]
