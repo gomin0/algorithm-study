e597432e554398f2f3dae75ae89acd616f944219
gomin0 <gomin0981113@naver.com>
2024-12-09 10:32:30 +0900

    [level 3] Title: 기지국 설치, Time: 5.67 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/README.md
+++ 프로그래머스/3/12979. 기지국 설치/README.md
@@ -1,10 +1,10 @@
 # [level 3] 기지국 설치 - 12979 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12979) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12979#) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 1.59 ms
+메모리: 10.5 MB, 시간: 5.67 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 18일 14:30:00
+2024년 12월 09일 10:32:25
 
 ### 문제 설명
 
--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.py
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.py
@@ -1,18 +1,27 @@
 def solution(n, stations, w):
     answer = 0
+
+    stations.sort()
     
-    position = 1
-    cover = 2 * w + 1
-    
-    for station in stations:
-        if position < station - w:
-            gap = station - w - position
-            answer += (gap + cover - 1) // cover
-            
-        position = station + w + 1
-        
-    if position <= n:
-        gap = n - position + 1
-        answer += (gap + cover - 1) // cover
-    
+    idx = 1
+    station = 0
+    while idx <= n:
+        point = stations[station]
+        if idx < (point - w):
+            answer += 1
+            idx += w + w + 1  # i + w 자리에 설치하고 idx 옮기기
+        elif (point - w) <= idx <= (point + w):
+            idx = point + w + 1
+            if station < len(stations) - 1:
+                station += 1
+        else:
+            if station < len(stations) - 1:
+                station += 1
+                if idx < stations[station]:
+                    answer += 1
+                    idx += w + w + 1
+            else:
+                answer += 1
+                idx += w + w + 1
+
     return answer
