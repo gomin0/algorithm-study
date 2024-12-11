641ae804e891c2360686a08c488012ad2d503780
gomin0 <gomin0981113@naver.com>
2024-12-11 10:49:48 +0900

    [level 3] Title: 스티커 모으기(2), Time: 63.77 ms, Memory: 16.7 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.py
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.py
@@ -5,18 +5,22 @@ def solution(sticker):
     
     if n == 1:
         return sticker[0]
+    elif n == 2:
+        return max(sticker)
     
-    dp1 = [0] * n  # 처음 거 먹는 경우
+    dp1 = [0 for _ in range(n)]
+    dp2 = [0 for _ in range(n)]
+    
+    # 첫번째꺼 먹기 -> 두번째꺼, 마지막꺼 못먹음
     dp1[0] = sticker[0]
-    dp1[1] = sticker[0]
+    dp1[1] = dp1[0]
+    for i in range(2, n-1):
+        dp1[i] = max(dp1[i-2] + sticker[i], dp1[i-1])
+    dp1[n-1] = dp1[n-2]
     
-    for i in range(2, n-1):  # 마지막 못먹음
-        dp1[i] = max(dp1[i-1], dp1[i-2] + sticker[i])
-        
-    dp2 = [0] *n  # 마지막 거 먹는 경우
+    # 찻번째꺼 안먹기
     dp2[1] = sticker[1]
-    
     for i in range(2, n):
-        dp2[i] = max(dp2[i-1], dp2[i-2] + sticker[i])
-    
-    return max(dp1[n-2], dp2[n-1])
+        dp2[i] = max(dp2[i-2] + sticker[i], dp2[i-1])
+
+    return max(dp1[n-1], dp2[n-1])
