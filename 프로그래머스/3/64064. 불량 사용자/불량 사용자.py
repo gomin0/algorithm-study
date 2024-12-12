034ab3c8cc6a9e18695f4fc31023a7f2abac8f25
gomin0 <gomin0981113@naver.com>
2024-12-12 10:08:42 +0900

    [level 3] Title: 불량 사용자, Time: 302.60 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
@@ -1,25 +1,24 @@
 from itertools import permutations
 
-def match(uid, bid):
+def check(uid, bid):
     if len(uid) != len(bid):
         return False
-    for i in range(len(uid)):
-        if uid[i] != bid[i] and bid[i] != '*':
+    for u, b in zip(uid, bid):
+        if u != b and b != '*':
             return False
     return True
 
 def solution(user_id, banned_id):
-    answer = 0
     
-    ban_list = []
+    ban_list = set()
     
-    for permutation in permutations(user_id, len(banned_id)):
-        is_match = True
-        for uid, bid in zip(permutation, banned_id):
-            if not match(uid, bid):
-                is_match = False
+    for up in permutations(user_id, len(banned_id)):
+        possible = True
+        for uid, bid in zip(up, banned_id):
+            if not check(uid, bid):
+                possible = False
                 break
-        if is_match:
-            ban_list.append(tuple(sorted(permutation)))
+        if possible:
+        	ban_list.add(tuple(sorted(up)))
     
-    return len(set(ban_list))
+    return len(ban_list)
