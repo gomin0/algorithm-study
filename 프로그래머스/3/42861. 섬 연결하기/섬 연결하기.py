6d65b9de4633917e1f9e6e94bfc115742bac910e
gomin0 <gomin0981113@naver.com>
2024-12-14 11:33:05 +0900

    [level 3] Title: 섬 연결하기, Time: 0.06 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
@@ -2,20 +2,15 @@ def find(parent, node):
     if node != parent[node]:
         parent[node] = find(parent, parent[node])
     return parent[node]
-        
 
-def union(x, y, parent, rank):
-    px = find(parent, x)
-    py = find(parent, y)
-    
-    if px != py:
-        if rank[px] > rank[py]:
-            parent[py] = px
-        elif rank[px] < rank[py]:
-            parent[px] = py
-        else:
-            parent[py] = px
-            rank[px] += 1
+def union(px, py, parent, rank):
+    if rank[px] > rank[py]:
+        parent[py] = px
+    elif rank[py] > rank[px]:
+        parent[px] = py
+    else:
+        parent[py] = px
+        rank[px] += 1
 
 def solution(n, costs):
     answer = 0
@@ -26,8 +21,10 @@ def solution(n, costs):
     costs.sort(key=lambda x:x[2])
     
     for x, y, cost in costs:
-         if find(parent, x) != find(parent, y):
-                union(x, y, parent, rank)
-                answer += cost
+        px = find(parent, x)
+        py = find(parent, y)
+        if px != py:
+            union(px, py, parent, rank)
+            answer += cost
     
     return answer
