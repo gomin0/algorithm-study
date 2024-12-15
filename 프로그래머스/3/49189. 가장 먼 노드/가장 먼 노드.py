2aa106ce1840cdd3be0f57fb7806cf2584ed15f2
gomin0 <gomin0981113@naver.com>
2024-12-15 11:10:36 +0900

    [level 3] Title: 가장 먼 노드, Time: 37.71 ms, Memory: 24.2 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -1,28 +1,27 @@
-from collections import deque, defaultdict
-
-def bfs(n, start, graph):
-    distance = [-1] * (n+1)
-    distance[start] = 0
-    queue = deque([start])
-    
-    while queue:
-        current_node = queue.popleft()
-        current_dist = distance[current_node]
-        
-        for next_node in graph[current_node]:
-            if distance[next_node] == -1:
-                distance[next_node] = current_dist + 1
-                queue.append(next_node)
-    return distance
+from collections import defaultdict, deque
 
 def solution(n, edge):
+    answer = 0
+    
+    def bfs(n, start, graph):
+        distance = [-1 for _ in range(n)]
+        distance[start] = 0
+        queue = deque([start])
+        
+        while queue:
+            node = queue.popleft()
+            dist = distance[node]
+            for next_node in graph[node]:
+                if distance[next_node] == -1:
+                    distance[next_node] = dist + 1
+                    queue.append(next_node)
+        return distance
     
     graph = defaultdict(list)
     for a, b in edge:
-        graph[a].append(b)
-        graph[b].append(a)
+        graph[a-1].append(b-1)
+        graph[b-1].append(a-1)
     
-    distance = bfs(n, 1, graph)
-    max_distance = max(distance)
+    distance = bfs(n, 0, graph)
     
-    return distance.count(max_distance)
+    return distance.count(max(distance))
