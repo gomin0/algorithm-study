31ebc33287235762b54dea04e4776b3d1b976ce7
gomin0 <gomin0981113@naver.com>
2024-12-16 11:05:02 +0900

    [level 3] Title: 징검다리 건너기, Time: 313.58 ms, Memory: 18.5 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/README.md
+++ 프로그래머스/3/64062. 징검다리 건너기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 징검다리 건너기 - 64062 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64062?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64062) 
 
 ### 성능 요약
 
-메모리: 18.5 MB, 시간: 370.44 ms
+메모리: 18.5 MB, 시간: 313.58 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 27일 11:18:22
+2024년 12월 16일 11:04:57
 
 ### 문제 설명
 
--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.py
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.py
@@ -1,26 +1,24 @@
-# 이분 탐색으로 건널 수 있는 사람 수 찾기
-
 def solution(stones, k):
-
-    def possible(mid):
-        count = 0  # 못 건너는 연속 돌
+    left = 0
+    right = max(stones)
+    
+    while left <= right:
+        mid = (left + right) // 2
+        possible = True
         
+        count = 0
         for stone in stones:
             if stone < mid:
                 count += 1
                 if count >= k:
-                    return False
+                    possible = False
+                    break
             else:
-                count = 0
-        return True
-    
-    left, right = 0, max(stones)
-    
-    while left <= right:
-        mid = (left + right) // 2
-        if possible(mid):
+                count = 0    
+        
+        if possible:
             left = mid + 1
         else:
             right = mid - 1
-            
+        
     return right
