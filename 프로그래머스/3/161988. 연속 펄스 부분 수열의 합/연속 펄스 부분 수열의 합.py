3023e8c72e938c7da6981cd8419a03317e52c2ad
gomin0 <gomin0981113@naver.com>
2024-12-18 10:38:43 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 499.46 ms, Memory: 86.5 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.py
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.py
@@ -6,11 +6,10 @@ def solution(sequence):
     pulse1 = [sequence[i] if i % 2 == 0 else -sequence[i] for i in range(n)]
     pulse2 = [-sequence[i] if i % 2 == 0 else sequence[i] for i in range(n)]
     
-    dp1 = [0] * n
-    dp2 = [0] * n
+    dp1 = [0 for _ in range(n)]
     dp1[0] = pulse1[0]
+    dp2 = [0 for _ in range(n)]
     dp2[0] = pulse2[0]
-    
     for i in range(1, n):
         dp1[i] = max(pulse1[i], dp1[i-1] + pulse1[i])
         dp2[i] = max(pulse2[i], dp2[i-1] + pulse2[i])
