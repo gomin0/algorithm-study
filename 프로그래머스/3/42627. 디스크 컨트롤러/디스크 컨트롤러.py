941412e638cb3ea0e717203b75ee371d1abe7d10
gomin0 <gomin0981113@naver.com>
2024-12-19 10:37:11 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.92 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -3,25 +3,23 @@ import heapq
 def solution(jobs):
     answer = 0
     
-    jobs.sort()
-    
-    current_time = 0
-    total_time = 0
     n = len(jobs)
+    current_time = 0
+    jobs.sort()
+    disk = []
+    
     idx = 0
-    
-    job_heap = []
-    
-    while idx < n or job_heap:
+    while idx < n or disk:
         while idx < n and jobs[idx][0] <= current_time:
-            heapq.heappush(job_heap, (jobs[idx][1], jobs[idx][0]))
+            start = jobs[idx][0]
+            time = jobs[idx][1]
+            heapq.heappush(disk, (time, start, idx))
             idx += 1
-            
-        if job_heap:
-            time, start_time = heapq.heappop(job_heap)
-            current_time += time
-            total_time += current_time - start_time
+        if disk:
+            t, s, i = heapq.heappop(disk)
+            current_time += t
+            answer += current_time - s
         else:
             current_time = jobs[idx][0]
-    
-    return total_time // n
+        
+    return answer // n
