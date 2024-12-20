8b20a165fd181787f9ceeb616cbe4bed67f793c3
gomin0 <gomin0981113@naver.com>
2024-12-20 11:45:03 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 7.04 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
@@ -4,29 +4,31 @@ def solution(board):
     answer = 0
     
     n = len(board)
-    costs = [[[float('inf')] * 4 for _ in range(n)] for _ in range(n)]
-    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    
+    prices = [[[float('inf')] * 4 for _ in range(n)] for _ in range(n)]
+    direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    
     queue = []
     
     for i in range(4):
-        costs[0][0][i] = 0
-        heapq.heappush(queue, (0, 0, 0, i))
+        prices[0][0][i] = 0
+        heapq.heappush(queue, (0, 0, 0, i)) # 가격, x, y, 방향
     
     while queue:
-        current_cost, x, y, direction = heapq.heappop(queue)
+        price, x, y, d = heapq.heappop(queue)
         
-        if x == n - 1 and y == n - 1:
-            return current_cost
+        if x == n-1 and y == n-1:
+            return price
         
-        for next_direction, (dx, dy) in enumerate(directions):
+        for i in range(4):
+            dx, dy = direction[i]
             nx, ny = x + dx, y + dy
-            if 0 <= nx < n and 0 <= ny < n and board[nx][ny] == 0:
-                next_cost = current_cost + 100
-                if direction != next_direction:
-                    next_cost += 500
-                
-                if next_cost < costs[nx][ny][next_direction]:
-                    costs[nx][ny][next_direction] = next_cost
-                    heapq.heappush(queue, (next_cost, nx, ny, next_direction))
+            if 0 <= nx < n and 0 <= ny < n and board[nx][ny] != 1:
+                next_price = price + 100
+                if d != i:
+                    next_price += 500
+                if next_price < prices[nx][ny][i]:
+                    prices[nx][ny][i] = next_price
+                    heapq.heappush(queue, (next_price, nx, ny, i))
     
     return min(costs[n-1][n-1])
