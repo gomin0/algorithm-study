fc79bafc3e8584919f57551810e05ddeb0fbb779
gomin0 <gomin0981113@naver.com>
2024-12-21 11:43:13 +0900

    [level 3] Title: 부대복귀, Time: 706.52 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/3/132266. 부대복귀/README.md
+++ 프로그래머스/3/132266. 부대복귀/README.md
@@ -1,10 +1,10 @@
 # [level 3] 부대복귀 - 132266 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132266?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132266) 
 
 ### 성능 요약
 
-메모리: 125 MB, 시간: 1134.07 ms
+메모리: 117 MB, 시간: 706.52 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 01일 12:17:43
+2024년 12월 21일 11:43:08
 
 ### 문제 설명
 
--- 프로그래머스/3/132266. 부대복귀/부대복귀.py
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.py
@@ -1,39 +1,30 @@
-import heapq
-from collections import defaultdict
-
-def dijkstra(start, graph, n):
-    distance = {i:float('inf') for i in range(1, n+1)}
-    distance[start] = 0
-    
-    queue = []
-    heapq.heappush(queue, (0, start))
-    while queue:
-        now_distance, now_node = heapq.heappop(queue)
-        if now_distance > distance[now_node]:
-            continue
-        for next_node in graph[now_node]:
-            dist = now_distance + 1
-            if dist < distance[next_node]:
-                distance[next_node] = dist
-                heapq.heappush(queue, (dist, next_node))
-    return distance
+from collections import deque, defaultdict
 
 def solution(n, roads, sources, destination):
     answer = []
     
-    graph = defaultdict(list)
+    def dijkstra(n, graph, start):
+        distance = [float('inf') for _ in range(n+1)]
+        distance[start] = 0
+        queue = deque([(start, 0)])
+        
+        while queue:
+            node, d = queue.popleft()
+            if d > distance[node]:
+                continue
+            for next_node in graph[node]:
+                if distance[next_node] > d + 1:
+                    distance[next_node] = d + 1
+                    queue.append((next_node, d+1))
+        return distance
     
+    graph = defaultdict(list)
     for a, b in roads:
         graph[a].append(b)
         graph[b].append(a)
-        
-    distance = dijkstra(destination, graph, n)
     
+    distance = dijkstra(n, graph, destination)
     for source in sources:
-        dist = distance[source]
-        if dist == float('inf'):
-            answer.append(-1)
-        else:
-            answer.append(dist)
+        answer.append(distance[source] if distance[source] != float('inf') else -1)
     
     return answer
