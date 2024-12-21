90c35ff26a1ef48dfb60a65a748738f425c09632
gomin0 <gomin0981113@naver.com>
2024-12-21 12:11:48 +0900

    [level 3] Title: 부대복귀, Time: 795.18 ms, Memory: 114 MB -BaekjoonHub

--- 프로그래머스/3/132266. 부대복귀/부대복귀.py
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.py
@@ -1,30 +1,23 @@
 from collections import deque, defaultdict
 
 def solution(n, roads, sources, destination):
-    answer = []
-    
-    def dijkstra(n, graph, start):
-        distance = [float('inf') for _ in range(n+1)]
-        distance[start] = 0
-        queue = deque([(start, 0)])
-        
-        while queue:
-            node, d = queue.popleft()
-            if d > distance[node]:
-                continue
-            for next_node in graph[node]:
-                if distance[next_node] > d + 1:
-                    distance[next_node] = d + 1
-                    queue.append((next_node, d+1))
-        return distance
-    
+
     graph = defaultdict(list)
     for a, b in roads:
         graph[a].append(b)
         graph[b].append(a)
     
-    distance = dijkstra(n, graph, destination)
-    for source in sources:
-        answer.append(distance[source] if distance[source] != float('inf') else -1)
+    distance = [-1 for _ in range(n+1)]
+    distance[destination] = 0
     
-    return answer
+    queue = deque([destination])
+    while queue:
+        node = queue.popleft()
+        dist = distance[node]
+        
+        for next_node in graph[node]:
+            if distance[next_node] == -1:
+                distance[next_node] = dist + 1
+                queue.append(next_node)
+    
+    return [distance[source] for source in sources]
