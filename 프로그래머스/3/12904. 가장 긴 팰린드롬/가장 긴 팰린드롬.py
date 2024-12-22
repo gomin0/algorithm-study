a3bccf57f78a66f23d41a8a7bee0d081e3d9cbd7
gomin0 <gomin0981113@naver.com>
2024-12-22 10:45:51 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 206.79 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.py
+++ 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.py
@@ -1,17 +1,16 @@
-
+def check(string, left, right):
+    n = len(string)
+    while 0 <= left and right < n and string[left] == string[right]:
+        left -= 1
+        right += 1
+    return right - left - 1
 
 def solution(s):
     answer = 0
-    
-    def palindrome(left, right):
-        while left >= 0 and right < len(s) and s[left] == s[right]:
-            left -= 1
-            right += 1
-        return right - left - 1
-    
-    for i in range(len(s)):
-        len1 = palindrome(i, i)
-        len2 = palindrome(i, i + 1)
-        answer = max(answer, len1, len2)
 
+    for i in range(len(s)):
+        len1 = max(answer, check(s, i, i))
+        len2 = max(answer, check(s, i, i+1))
+        answer = max(answer, len1, len2)
+        
     return answer
