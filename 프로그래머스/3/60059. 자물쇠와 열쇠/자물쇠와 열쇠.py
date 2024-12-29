f7c16ca0ffe39a894880cdd3a68d06a05cfa247f
gomin0 <gomin0981113@naver.com>
2024-12-29 11:51:34 +0900

    [level 3] Title: 자물쇠와 열쇠, Time: 623.27 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.py
+++ 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.py
@@ -1,42 +1,43 @@
-def key_rotation(key):
+def key_rotation(key):  # 키 돌리기
     m = len(key)
     rotation = []
     
     for i in range(m):
         key_line = []
         for j in range(m):
-            key_line.append(key[m - 1 - j][i])
+            key_line.append(key[m-1-j][i])
         rotation.append(key_line)
     return rotation
 
-def check(key, expanded_lock, x, y, n, m):
+def check(key, expanded_lock, a, b, n, m):
     test_lock = [row[:] for row in expanded_lock]
     
     for i in range(m):
         for j in range(m):
-            if 0 <= x + i < n + 2 * (m - 1) and 0 <= y + j < n + 2 * (m - 1):
-                test_lock[x+i][y+j] += key[i][j]
-    
+            if 0 <= a + i < n + 2 * (m - 1) and 0 <= b + j < n + 2 * (m - 1):
+                test_lock[a+i][b+j] += key[i][j]
+                
     for i in range(n):
         for j in range(n):
             if test_lock[m-1+i][m-1+j] != 1:
                 return False
     return True
-    
 
 def solution(key, lock):
+    
     m, n = len(key), len(lock)
-    expanded_lock = [[0] * (n + 2 * (m-1)) for _ in range(n + 2 * (m-1))]
+    expanded_len = n + 2 * (m - 1)
+    expanded_lock = [[0] * expanded_len for _ in range(expanded_len)]
     
     for i in range(n):
         for j in range(n):
-            expanded_lock[m-1+i][m-1+j] = lock[i][j]
+            expanded_lock[m-1+i][m-1+j] = lock[i][j]  # 기존 자물쇠 값 이식
     
-    for _ in range(4):
+    for _ in range(4):  # 네 방향으로 열쇠 돌리기
         key = key_rotation(key)
-        for x in range(n+m-1):
-            for y in range(n+m-1):
-                if check(key, expanded_lock, x, y, n, m):
+        for a in range(n+m-1):
+            for b in range(n+m-1):
+                if check(key, expanded_lock, a, b, n, m):
                     return True
     
     return False
