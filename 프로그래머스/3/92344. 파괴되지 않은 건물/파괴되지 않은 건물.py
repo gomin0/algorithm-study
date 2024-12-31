b70df00969c99496d43e65098da614d0c7a44c84
gomin0 <gomin0981113@naver.com>
2024-12-31 12:19:30 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 722.10 ms, Memory: 135 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.py
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.py
@@ -1,15 +1,12 @@
-# 누적합 이용하기 -> 스킬을 한번에 처리
-
 def solution(board, skill):
     answer = 0
-    n = len(board)
-    m = len(board[0])
+    
+    n, m = len(board), len(board[0])
     skill_sum = [[0] * (m+1) for _ in range(n+1)]
     
     for skill_type, x1, y1, x2, y2, degree in skill:
         if skill_type == 1:
             degree = -degree
-        
         skill_sum[x1][y1] += degree
         skill_sum[x1][y2+1] -= degree
         skill_sum[x2+1][y1] -= degree
@@ -17,15 +14,14 @@ def solution(board, skill):
     
     for i in range(n):
         for j in range(m):
-            skill_sum[i][j+1] += skill_sum[i][j]
-            
+            skill_sum[i][j+1] += skill_sum[i][j]  # 가로 방향 누적 합
     for j in range(m):
         for i in range(n):
-            skill_sum[i+1][j] += skill_sum[i][j]
-            
+            skill_sum[i+1][j] += skill_sum[i][j]  # 세로 방향 누적 합
+    
     for i in range(n):
         for j in range(m):
             if board[i][j] + skill_sum[i][j] > 0:
                 answer += 1
-        
+    
     return answer
