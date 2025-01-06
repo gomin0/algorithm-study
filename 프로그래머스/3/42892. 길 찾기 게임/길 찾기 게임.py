669b3a3fcab9f4132a66ca2636aaec70e1fdc3d6
gomin0 <gomin0981113@naver.com>
2025-01-06 10:39:56 +0900

    [level 3] Title: 길 찾기 게임, Time: 411.64 ms, Memory: 21.8 MB -BaekjoonHub

--- 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.py
+++ 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.py
@@ -1,47 +1,37 @@
 import sys
-sys.setrecursionlimit(10**6)  # 파이썬의 기본 재귀 깊이 제한은 1000 노드가 최대 10,000개까지 있을 수 있어서 재귀 제한을 늘려주기
-
-class Node:
-    def __init__(self, info, idx):
-        self.x = info[0]
-        self.y = info[1]
-        self.idx = idx
-        self.left = None
-        self.right = None
+sys.setrecursionlimit(10**6)  # 파이썬의 기본 재귀 깊이 제한을 늘림
 
 def solution(nodeinfo):
-    nodes = [(x, y, i+1) for i, (x, y) in enumerate(nodeinfo)]
-    nodes.sort(key=lambda x: (-x[1], x[0]))
+    nodes = [{'x': x, 'y': y, 'idx': i + 1} for i, (x, y) in enumerate(nodeinfo)]
+    nodes.sort(key=lambda n: (-n['y'], n['x']))  # y 내림차순, x 오름차순으로 정렬
     
     def make_tree(nodes):
         if not nodes:
             return None
         
-        node_x, node_y, idx = nodes[0]
-        node = Node([node_x, node_y], idx)
-            
-        left_nodes = [n for n in nodes[1:] if n[0] < node_x]
-        right_nodes = [n for n in nodes[1:] if n[0] > node_x]
+        root = nodes[0]  # 가장 위(y가 가장 크고, x가 작은 노드)가 루트
+        left_nodes = [node for node in nodes[1:] if node['x'] < root['x']]
+        right_nodes = [node for node in nodes[1:] if node['x'] > root['x']]
         
-        node.left = make_tree(left_nodes)
-        node.right = make_tree(right_nodes)
+        root['left'] = make_tree(left_nodes)  # 왼쪽 서브트리 생성
+        root['right'] = make_tree(right_nodes)  # 오른쪽 서브트리 생성
         
-        return node
+        return root
     
     def preorder(node, traversal):
         if node:
-            traversal.append(node.idx)
-            preorder(node.left, traversal)
-            preorder(node.right, traversal)
+            traversal.append(node['idx'])  # 현재 노드 방문
+            preorder(node['left'], traversal)  # 왼쪽 서브트리 탐색
+            preorder(node['right'], traversal)  # 오른쪽 서브트리 탐색
         return traversal
     
     def postorder(node, traversal):
         if node:
-            postorder(node.left, traversal)
-            postorder(node.right, traversal)
-            traversal.append(node.idx)
+            postorder(node['left'], traversal)  # 왼쪽 서브트리 탐색
+            postorder(node['right'], traversal)  # 오른쪽 서브트리 탐색
+            traversal.append(node['idx'])  # 현재 노드 방문
         return traversal
     
-    root = make_tree(nodes)
+    root = make_tree(nodes)  # 트리 생성
     
     return [preorder(root, []), postorder(root, [])]
