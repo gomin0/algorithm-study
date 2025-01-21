754e849e97813b6f0ef0161c2560a556457f1df5
gomin0 <gomin0981113@naver.com>
2025-01-21 11:18:58 +0900

    [level 3] Title: 기둥과 보 설치, Time: 208.73 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/60061. 기둥과 보 설치/기둥과 보 설치.py
+++ 프로그래머스/3/60061. 기둥과 보 설치/기둥과 보 설치.py
@@ -1,30 +1,28 @@
-def is_valid(structures):
+def check(structures):  # 현재 구조물이 유효한지 체크
     for x, y, a in structures:
-        if a == 0:  # 기둥
-            # 바닥 위, 다른 기둥 위, 보 위
-            if y == 0 or (x, y-1, 0) in structures or (x-1, y, 1) in structures or (x, y, 1) in structures:
-                continue
-            return False
+        if a == 0:  #기둥
+            if y == 0 or (x, y-1, 0) in structures or (x-1, y, 1) in structures or (x,y, 1) in structures:  # 바닥 위 or 다른 기둥 위 or 왼쪽 보 위 or 오른쪽 보 위
+                continue  # 통과
+            return False  # 유효하지 않음
         elif a == 1:  # 보
-            # 한쪽 끝 기둥 위, 양쪽 끝이 다른 보와 연결
-            if (x, y-1, 0) in structures or (x+1, y-1, 0) in structures or ((x-1, y, 1) in structures and (x+1, y, 1) in structures):
-                continue
-            return False
-    return True
-
-def solution(n, build_frame):    
+            if (x, y-1, 0) in structures or (x+1, y-1, 0) in structures or ((x-1, y, 1) in structures and (x+1, y, 1) in structures):  # 왼쪽 기둥 위 or 오른쪽 기둥 위 or 양쪽이 보로 연결
+                continue  # 통과
+            return False  # 유효하지 않음
+    return True  # 유효한 경우
+    
+def solution(n, build_frame):
     structures = set()
     
     for x, y, a, b in build_frame:
         if b == 0:  # 삭제
-            structures.remove((x, y, a))
-            if not is_valid(structures):
-                structures.add((x, y, a))
+            structures.remove((x, y, a))  # 삭제
+            if not check(structures):  # 유효하지 않으면
+                structures.add((x, y, a))  # 복구
         else:  # 설치
-            structures.add((x, y, a))
-            if not is_valid(structures):
-                structures.remove((x, y, a))
-    
-    answer = sorted(structures, key=lambda item: (item[0], item[1], item[2]))
-    
+            structures.add((x, y, a))  # 설치
+            if not check(structures):  # 유효하지 않으면
+                structures.remove((x, y, a))  # 삭제
+        
+        answer = sorted(structures, key=lambda item: (item[0], item[1], item[2]))
+        
     return answer
