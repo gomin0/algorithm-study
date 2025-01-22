d01921e982493645aa7c6ddd213a6e5ba85c484a
gomin0 <gomin0981113@naver.com>
2025-01-22 12:04:31 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.21 ms, Memory: 56.6 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 11.65 ms
+메모리: 56.6 MB, 시간: 7.21 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 04일 21:41:32
+2025년 01월 22일 12:04:26
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,40 +1,46 @@
 import java.util.*;
+
 class Solution {
-    static int dx[] = {-1, 1, 0, 0};
-    static int dy[] = {0, 0, -1, 1};
-    static boolean visited[];
     public int solution(int[][] maps) {
         int answer = 0;
-        int[][] visited = new int[maps.length][maps[0].length];
         
-        bfs(maps, visited);
+        int n = maps.length;
+        int m = maps[0].length;
         
-        answer = visited[maps.length - 1][maps[0].length - 1];
-        if(answer == 0) {
-            return -1;
-        }
-        return answer;
-    }
-    
-    static void bfs (int[][] maps, int[][] visited) {
-        Queue <int[]> q = new LinkedList<>();
-        q.add(new int[] {0, 0});
-        visited[0][0] = 1;
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
         
-        while(!q.isEmpty()) {
-            int[] now = q.poll();
-            int x = now[0];
-            int y = now[1];
-            
-            for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
+        boolean[][] visited = new boolean[n][m];
+        Queue<int[]> queue = new LinkedList<>();
+        
+        queue.offer(new int[]{0, 0});
+        visited[0][0] = true;
+        
+        int distance = 1;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                int[] current = queue.poll();
+                int x = current[0];
+                int y = current[1];
                 
-                if (nx >= 0 && nx < maps.length && ny >= 0 && ny < maps[0].length && visited[nx][ny] == 0 && maps[nx][ny] == 1) {
-                    visited[nx][ny] = visited[x][y] + 1;
-                    q.add(new int[]{nx, ny});
+                if (x == n-1 && y == m-1) {
+                    return distance;
+                }
+                
+                for (int j = 0; j < 4; j++) {
+                    int nx = x + dx[j];
+                    int ny = y + dy[j];
+                    
+                    if (nx >= 0 && nx < n && ny >= 0 && ny < m && !visited[nx][ny] && maps[nx][ny] == 1) {
+                        queue.offer(new int[]{nx, ny});
+                        visited[nx][ny] = true;
+                    }
                 }
             }
+            distance++;
         }
+        
+        return -1;
     }
 }
