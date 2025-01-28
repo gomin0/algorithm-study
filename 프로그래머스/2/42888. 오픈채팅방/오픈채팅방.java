f0f4d5a9cdb69cfe64984201b4b1a58f93042a15
gomin0 <gomin0981113@naver.com>
2025-01-28 10:24:12 +0900

    [level 2] Title: 오픈채팅방, Time: 138.91 ms, Memory: 180 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -1,34 +1,31 @@
 import java.util.*;
 
 class Solution {
-    public String[] solution(String[] record) {        
+    public ArrayList<String> solution(String[] record) {        
         Map<String, String> name = new HashMap<>();
-        List<String[]> messages = new ArrayList<>();
+        ArrayList<String> answer = new ArrayList<>();
 
         for (String r : record) {
             String[] parts = r.split(" ");
             String command = parts[0];
             String uid = parts[1];
-            
-            if (command.equals("Enter")) {
-                String nickname = parts[2];
-                name.put(uid, nickname);
-                messages.add(new String[]{uid, "님이 들어왔습니다."});
-            } else if (command.equals("Leave")) {
-                messages.add(new String[]{uid, "님이 나갔습니다."});
-            } else {
+            if (!command.equals("Leave")) {
                 String nickname = parts[2];
                 name.put(uid, nickname);
             }
         }
         
-        String[] answer = new String[messages.size()];
-        int idx = 0;
-        for (String[] message : messages) {
-            String userId = message[0];
-            String action = message[1];
-            String nickname = name.get(userId);
-            answer[idx++] = nickname+action;
+        for (String r : record) {
+            String[] parts = r.split(" ");
+            String command = parts[0];
+            String uid = parts[1];
+            String nickname = name.get(uid);
+            
+            if (command.equals("Enter")) {
+                answer.add(nickname + "님이 들어왔습니다.");
+            } else if (command.equals("Leave")) {
+                answer.add(nickname + "님이 나갔습니다.");
+            }
         }
         
         return answer;
