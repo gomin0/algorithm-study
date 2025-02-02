ad7416bf3d30894923719a294b6c335f34d66e19
gomin0 <gomin0981113@naver.com>
2025-02-02 13:56:52 +0900

    [level 3] Title: 등산코스 정하기, Time: 503.30 ms, Memory: 91 MB -BaekjoonHub

--- 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.py
+++ 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.py
@@ -2,44 +2,40 @@ import heapq
 from collections import defaultdict
 
 def solution(n, paths, gates, summits):
+    answer = []
     
     graph = defaultdict(list)
+    
     for a, b, c in paths:
         graph[a].append((b, c))
         graph[b].append((a, c))
+
+    summits_set = set(summits)
     
-    summit_set = set(summits)
+    pq = []
+    intensity = [float('inf') for _ in range(n+1)]
     
-    def dijkstra():
-        intensity = [float('inf')] * (n + 1)
-        pq = []  # (현재까지의 intensity, 현재 노드)
+    for gate in gates:
+        heapq.heappush(pq, (0, gate))
+        intensity[gate] = 0
+
+    while pq:
+        cost, node = heapq.heappop(pq)
         
-        for gate in gates:
-            heapq.heappush(pq, (0, gate))
-            intensity[gate] = 0
+        if cost > intensity[node]:
+            continue
+        if node in summits_set:
+            continue
         
-        while pq:
-            cost, node = heapq.heappop(pq)
-            
-            # 이미 더 좋은 경로가 있으면 무시 (중복 계산 방지)
-            if cost > intensity[node]:
-                continue
-                
-            if node in summit_set:
-                continue  # 최적의 상태로 도달한거라 왔던 길 그대로 돌아가면 됨
-            
-            for next_node, weight in graph[node]:
-                new_intensity = max(cost, weight)
-                
-                if new_intensity < intensity[next_node]:
-                    intensity[next_node] = new_intensity
-                    heapq.heappush(pq, (new_intensity, next_node))
-        return intensity
+        for next_node, next_cost in graph[node]:
+            new_intensity = max(cost, next_cost)
+            if new_intensity < intensity[next_node]:
+                intensity[next_node] = new_intensity
+                heapq.heappush(pq, (new_intensity, next_node))
     
-    result = dijkstra()
     best_summit = (-1, float('inf'))
     for summit in sorted(summits):
-        if result[summit] < best_summit[1]:
-            best_summit = (summit, result[summit])
+        if intensity[summit] < best_summit[1]:
+            best_summit = (summit, intensity[summit])
     
     return list(best_summit)
