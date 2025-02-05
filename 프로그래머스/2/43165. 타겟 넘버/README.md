b94ec0fcb9c55d86777f08b8dc113924d4364e73
gomin0 <gomin0981113@naver.com>
2025-02-05 09:44:11 +0900

    [level 2] Title: 타겟 넘버, Time: 387.27 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 509.47 ms
+메모리: 10.1 MB, 시간: 387.27 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 04일 12:13:02
+2025년 02월 05일 09:44:06
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,12 +1,14 @@
 def solution(numbers, target):
     answer = 0
     
+    n = len(numbers)
+    
     def dfs(index, current):
-        if index == len(numbers):
+        if index == n:
             if current == target:
                 return 1
             else:
                 return 0
-        return dfs(index + 1, current + numbers[index]) + dfs(index + 1, current - numbers[index])
+        return dfs(index+1, current + numbers[index]) + dfs(index+1, current - numbers[index])
     
     return dfs(0, 0)
