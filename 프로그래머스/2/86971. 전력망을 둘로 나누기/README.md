e97d0c329e7b226f28f1561918db87fbc0c2451f
gomin0 <gomin0981113@naver.com>
2025-02-21 21:01:23 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.62 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/README.md
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전력망을 둘로 나누기 - 86971 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86971) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86971?language=python3) 
 
 ### 성능 요약
 
-메모리: 85.4 MB, 시간: 11.98 ms
+메모리: 10.3 MB, 시간: 2.62 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 09일 11:51:44
+2025년 02월 21일 21:01:18
 
 ### 문제 설명
 
--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,38 +1,41 @@
-def solution(n, wires):
-    answer = n
-    
-    graph = [[0] * (n+1) for _ in range(n+1)]
-    
-    def bfs(start):
-        visited = [False] * (n+1)
-        count = 1
-        stack = []
-        visited[start] = True
-        stack.append(start)
-        
-        while stack:
-            temp = stack.pop(0)
-            
-            for q in range(1, n + 1):
-                if graph[temp][q] == 1 and not visited[q]:
-                    visited[q] = True
-                    stack.append(q)
-                    count += 1
-        return abs(count - (n - count))
-    
-    for i in wires:
-        a, b = i[0], i[1]
-        graph[a][b] = 1
-        graph[b][a] = 1
+from collections import deque, defaultdict
 
-    for wire in wires:
-        c, d = wire[0], wire[1]
-        graph[c][d] = 0 # 하나 빼기
-        graph[d][c] = 0
+def bfs(n, graph, start):
+    count = 1
+    q = deque([start])
+    visited = {i:False for i in range(1, n+1)}
+    visited[start] = True
+
+    while q:
+        node = q.popleft()
+        for next_node in graph[node]:
+            if not visited[next_node]:
+                visited[next_node] = True
+                count += 1
+                q.append(next_node)
+    
+    return abs(count - (n-count))
+            
+            
+def solution(n, wires):
+    answer = float('inf')
+    
+    graph = defaultdict(list)
+    
+    
+    for a, b in wires:
+        graph[a].append(b)
+        graph[b].append(a)
+                
+    for a, b in wires:
+        # 끊기
+        graph[a].remove(b)
+        graph[b].remove(a)
+    
+        answer = min(bfs(n, graph, a), answer)
         
-        answer = min(answer, bfs(c))
-        
-        graph[c][d] = 1 # 다시 복구
-        graph[d][c] = 1
+        # 복구
+        graph[a].append(b)
+        graph[b].append(a)
         
     return answer
