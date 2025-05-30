e99624204583dfaee9d5669e10579cf681bda126
gomin0 <gomin0981113@naver.com>
2025-05-30 10:21:41 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.06 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
@@ -1,7 +1,5 @@
 def solution(s):
     nums: list[int] = [int(n) for n in s.split(" ")]
-    max_num: int = max(nums)
-    min_num: int = min(nums)
         
-    answer: str = str(min_num) + " " + str(max_num)
+    answer: str = str(min(nums)) + " " + str(max(nums))
     return answer
