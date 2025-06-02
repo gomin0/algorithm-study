678f75e3517f7bb433e07c9f25e2eaf1f5e1f4d1
gomin0 <gomin0981113@naver.com>
2025-06-02 15:09:51 +0900

    [level 2] Title: 귤 고르기, Time: 48.00 ms, Memory: 25.5 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -1,17 +1,14 @@
+from collections import Counter
+
 def solution(k, tangerine):
-    answer = 0
-    tangerine_count = {}
-    for i in tangerine:
-        if i in tangerine_count:
-            tangerine_count[i] += 1
+    answer: int = 0
+    tangerine_count: list[tuple] = Counter(tangerine).most_common()
+    
+    for i in range(len(tangerine_count)):
+        if k > 0:
+            k -= tangerine_count[i][1]
+            answer += 1
         else:
-            tangerine_count[i] = 1
-    
-    sorted_tangerine = sorted(tangerine_count.values(), reverse=True)
-    
-    for j in sorted_tangerine:
-        k -= j
-        answer += 1
-        if k <= 0:
-            return answer
+            break
+
     return answer
