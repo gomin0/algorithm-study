5767484f5cac7ff53c918cccbb1d1cbf85f24387
gomin0 <gomin0981113@naver.com>
2025-06-04 10:33:56 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 266.92 ms, Memory: 43 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -3,8 +3,15 @@ def solution(elements):
     length: int = len(elements)
     elements *= 2
     
-    for i in range(length * 2):
-        for j in range(1, length + 1):
-            answer.add(sum(elements[i:i+j]))
+    # 누적합 배열
+    prefix: list[int] = [0] * (length * 2 + 1)
+    for i in range(1, length * 2):
+        prefix[i] = prefix[i-1] + elements[i]
+    
+    for start in range(length):
+        for size in range(1, length + 1):
+            end: int = start + size
+            total_sum: int = prefix[end] - prefix[start]
+            answer.add(total_sum)
     
     return len(answer)
