72000589828d52d4e47e1ad80449da8c78389697
gomin0 <gomin0981113@naver.com>
2025-06-04 10:54:54 +0900

    [level 1] Title: 약수의 합, Time: 0.04 ms, Memory: 90.8 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/README.md
+++ 프로그래머스/1/12928. 약수의 합/README.md
@@ -1,10 +1,10 @@
 # [level 1] 약수의 합 - 12928 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12928?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12928) 
 
 ### 성능 요약
 
-메모리: 80.5 MB, 시간: 0.09 ms
+메모리: 90.8 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 16일 14:44:54
+2025년 06월 04일 10:54:52
 
 ### 문제 설명
 
--- 프로그래머스/1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -2,16 +2,16 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
         int num = (int) Math.sqrt(n);
-        
+    
         for (int i = 1; i <= num; i++) {
             if (n % i == 0) {
                 answer += i;
-                if (i != n/i){
-                    answer += n / i;
+                int num2 = n / i;
+                if (num2 != i) {
+                    answer += num2;
                 }
             }
         }
-        
         return answer;
     }
 }
