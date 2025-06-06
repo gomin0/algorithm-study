5c98c055f829a5f4a9fc866eb601565704eadda6
gomin0 <gomin0981113@naver.com>
2025-06-06 13:42:04 +0900

    [level 3] Title: 네트워크, Time: 1.05 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,24 +1,23 @@
-from collections import deque, defaultdict
+from collections import deque
 
 def solution(n, computers):
     answer = 0
     
     def bfs(start):
-        queue = deque([start])
+        queue: deque[int] = deque([start])
         visited.add(start)
         
         while queue:
             node = queue.popleft()
             for i in range(n):
-                if i != node and computers[node][i] == 1:
-                    if not i in visited:
-                        visited.add(i)
-                        queue.append(i)
-            
-    visited = set()
+                if i != node and computers[node][i] == 1 and i not in visited:
+                    visited.add(i)
+                    queue.append(i)
+        
+    visited: set[int] = set()
     for i in range(n):
         if not i in visited:
             bfs(i)
             answer += 1
-    
+        
     return answer
