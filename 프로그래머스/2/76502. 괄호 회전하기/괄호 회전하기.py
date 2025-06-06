83bd634b9b535df0b4cfb7bc2968aaa28ac723e2
gomin0 <gomin0981113@naver.com>
2025-06-06 13:18:52 +0900

    [level 2] Title: 괄호 회전하기, Time: 91.83 ms, Memory: 9.39 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -1,61 +1,31 @@
 def solution(s):
-    answer = 0
-    
-    length = len(s)
-    s = s * 2
+    answer: int = 0
+    length: int = len(s)
+    s *= 2
     
     for i in range(length):
-        stack = []
-        valid = True
-        for j in range(i, i + length):
-            if s[j] in '[{(':
-                stack.append(s[j])
-            elif s[j] in ']})':
-                if not stack: # 비어있는 경우
-                    valid = False
+        correct: bool = True
+        stack: list[str] = []
+        for j in range(i, i+length):
+            c: str = s[j]
+            if c in "({[":
+                stack.append(c)
+            else:
+                if not stack:
+                    correct = False
                     break
-                if s[j] == ']' and stack[-1] != '[':
-                    valid = False
+                elif c == ')' and stack[-1] != '(':
+                    correct = False
                     break
-                if s[j] == '}' and stack[-1] != '{':
-                    valid = False
+                elif c == '}' and stack[-1] != '{':
+                    correct = False
                     break
-                if s[j] == ')' and stack[-1] != '(':
-                    valid = False
+                elif c == ']' and stack[-1] != '[':
+                    correct = False
                     break
-                stack.pop()
-        
-        if valid and not stack:
+                else:
+                    stack.pop()
+        if correct and not stack:
             answer += 1
-    
+            
     return answer
-
-# {(}) 안됨
-# def solution(s):
-#     answer = 0
-#     length = len(s)
-#     s = s*2
-    
-#     for i in range(length):
-#         small = 0
-#         middle = 0
-#         big = 0
-#         for j in range(i, i + length):
-#             if s[j] == '[':
-#                 big += 1
-#             elif s[j] == ']':
-#                 big -= 1
-#             elif s[j] == '{':
-#                 middle += 1
-#             elif s[j] == '}':
-#                 middle -= 1
-#             elif s[j] == '(':
-#                 small += 1
-#             elif s[j] == ')':
-#                 small -= 1
-#             if big < 0 or middle < 0 or small < 0:
-#                 break
-#         if big == 0 and middle == 0 and small == 0:
-#             answer += 1
-    
-#     return answer
