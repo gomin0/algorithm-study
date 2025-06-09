5d0e5501507787ca53ec07a6a8d3f6fe152ef111
gomin0 <gomin0981113@naver.com>
2025-06-09 21:58:11 +0900

    [level 3] Title: 이중우선순위큐, Time: 437.68 ms, Memory: 15.5 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,23 +1,28 @@
 import heapq
 
 def solution(operations):
-    answer = []
-    min_heap = []
-    max_heap = []
+    answer: list[int] = [0, 0]
     
-    for operation in operations:
-        op, n = operation.split()
-        n = int(n)
-        if op == 'I':
-            heapq.heappush(min_heap, n)
-            heapq.heappush(max_heap, -n)
-        else:
-            if n == 1 and max_heap:
-                min_heap.remove(-heapq.heappop(max_heap))
-            elif n == -1 and min_heap:
-                max_heap.remove(-heapq.heappop(min_heap))
+    max_heap: list[int] = []
+    min_heap: list[int] = []
     
-    if min_heap and max_heap:
-        return [-max_heap[0], min_heap[0]]
-    else:
-        return [0, 0]
+    for op in operations:
+        command: str
+        num: str
+        command, num = op.split()
+        
+        if command == 'I':
+            heapq.heappush(max_heap, -int(num))
+            heapq.heappush(min_heap, int(num))
+        elif num == "1" and max_heap:
+            min_heap.remove(-heapq.heappop(max_heap))
+        elif num == "-1" and min_heap:
+            max_heap.remove(-heapq.heappop(min_heap))
+    
+    if max_heap:
+        answer[0] = -heapq.heappop(max_heap)
+    if min_heap:
+        answer[1] = heapq.heappop(min_heap)
+    
+    return answer
+    
