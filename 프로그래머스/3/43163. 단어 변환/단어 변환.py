45c6d39107b605cb2ad874f848bb548ab848c5a3
gomin0 <gomin0981113@naver.com>
2025-06-11 10:13:31 +0900

    [level 3] Title: 단어 변환, Time: 0.61 ms, Memory: 9.15 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,48 +1,34 @@
-from collections import defaultdict, deque
+from collections import deque
 
-def can_change(str1, str2):
-    count = 0
-    for i in range(len(str1)):
-        if str1[i] != str2[i]:
-            count += 1
-        if count > 1:
-            return False
-    
-    return count == 1
+def check_convertable(word1, word2):
+    diff: int = 0
+    for c1, c2 in zip(word1, word2):
+        if c1 != c2:
+            diff += 1
+    return diff == 1
+
 
 def solution(begin, target, words):
-    answer = 0
+    answer: int = 0
     
-    if target not in words:
-        return 0
-    
-    change_dict = defaultdict(list)
-    
-    def bfs(start, change_dict):
-        queue = deque([(start, 0)])
-        visited = set([start])
+    def bfs(start):
+        queue: list[str] = deque([(start, 0)])
+        visited.add(start)
         
         while queue:
-            start_str, count = queue.popleft()
-            if start_str == target:
-                return count
+            now: str
+            step: int
+            now, step = queue.popleft()
+            if now == target:
+                return step
             
-            for next_str in change_dict[start_str]:
-                if next_str not in visited:
-                    queue.append((next_str, count + 1))
-                    visited.add(next_str)
-    
+            for word in words:
+                if word not in visited and check_convertable(now, word):
+                    visited.add(word)
+                    queue.append((word, step + 1))
         return 0
     
+    visited: set[str] = set()
+    answer = bfs(begin)
     
-    for word in words:
-        if can_change(begin, word):
-            change_dict[begin].append(word)
-    
-    for i in range(len(words) - 1):
-        for j in range(i + 1, len(words)):
-            if can_change(words[i], words[j]):
-                change_dict[words[i]].append(words[j])
-                change_dict[words[j]].append(words[i])
-    
-    return bfs(begin, change_dict)
+    return answer
