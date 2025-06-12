b9b2a7ba102ecfab2fb5c7995e7d3d4a1b9db9dc
gomin0 <gomin0981113@naver.com>
2025-06-12 13:54:58 +0900

    [level 3] Title: 최고의 집합, Time: 0.94 ms, Memory: 9.69 MB -BaekjoonHub

--- 프로그래머스/3/12938. 최고의 집합/최고의 집합.py
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.py
@@ -1,17 +1,10 @@
 def solution(n, s):
-    
-    if s < n:
+    div: int = s // n
+    if div == 0:
         return [-1]
+    rest: int = s % n
+    answer: list[int] = [div] * n
+    for i in range(rest):
+        answer[n - i - 1] += 1
     
-    a = s // n
-    b = s % n
-    
-    answer = [a for _ in range(n)]
-    for i in range(n - 1, -1, -1):
-        if b > 0:
-            answer[i] += 1
-            b -= 1
-        else:
-            break
-        
     return answer
