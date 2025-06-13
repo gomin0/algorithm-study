1702d1756603922875da1ede7739f7bebeefddad
gomin0 <gomin0981113@naver.com>
2025-06-13 21:46:56 +0900

    [level 3] Title: 단속카메라, Time: 0.91 ms, Memory: 9.62 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.py
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.py
@@ -1,13 +1,15 @@
 def solution(routes):
-    answer = 1
-    
+    answer: int = 1
     routes.sort(key=lambda x: x[1])
-    point = routes[0][1]
+    camera_point: int = routes[0][1]
     
     for i in range(1, len(routes)):
-        a, b = routes[i]
-        if point < a:
+        start: int
+        end: int
+        start, end = routes[i]
+        
+        if camera_point < start:
             answer += 1
-            point = b
+            camera_point = end
     
     return answer
