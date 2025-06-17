469f15bb5e9f0e9513b4ef9b5e90068f2bf33420
gomin0 <gomin0981113@naver.com>
2025-06-17 11:32:27 +0900

    [level 3] Title: 스티커 모으기(2), Time: 61.80 ms, Memory: 15.8 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.py
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.py
@@ -1,24 +1,20 @@
-def solution(sticker):
-    answer = 0
+def solution(sticker) -> int:
+    length: int = len(sticker)
     
-    n = len(sticker)
-
-    if n <= 3:
-        return max(sticker)
+    if length == 1:
+        return sticker[0]
     
-    dp1 = [0 for _ in range(n)]
-    dp2 = [0 for _ in range(n)]
-    
-    # 첫번째꺼 먹기 -> 두번째꺼, 마지막꺼 못먹음
+    dp1: list[int] = [0] * length
+    dp2: list[int] = [0] * length
     dp1[0] = sticker[0]
     dp1[1] = dp1[0]
-    for i in range(2, n-1):
-        dp1[i] = max(dp1[i-2] + sticker[i], dp1[i-1])
-    dp1[n-1] = dp1[n-2]
-    
-    # 찻번째꺼 안먹기
     dp2[1] = sticker[1]
-    for i in range(2, n):
+    
+    for i in range(2, length - 1):
+        dp1[i] = max(dp1[i-2] + sticker[i], dp1[i-1])
+    
+    for i in range(2, length):
         dp2[i] = max(dp2[i-2] + sticker[i], dp2[i-1])
-
-    return max(dp1[n-1], dp2[n-1])
+    
+    answer: int = max(dp1[-2], dp2[-1])
+    return answer
