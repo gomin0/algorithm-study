9723ae949a39672faaa33e130ba1ebde79517839
gomin0 <gomin0981113@naver.com>
2025-06-17 16:24:57 +0900

    [level 2] Title: H-Index, Time: 0.92 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,34 +1,13 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[] citations) {
+        int len = citations.length;
         Arrays.sort(citations);
-        
-        int n = citations.length;
-        for (int i = 0; i < n; i++) {
-            int h = n - i;
-            if (citations[i] >= h) {
-                return h;
-            }
+        for (int i = 0; i < len; i++) {
+            if (citations[len-1-i] <= i)
+                return i;
         }
-        
-        return 0;
-        
-        // for(int i = citations.length - 1; i >= 0; i--) {
-        //     int h = citations[i];
-        //     int count1 = 0;
-        //     int count2 = 0;
-        //     for (int j = citations.length - 1; j >= 0; j--) {
-        //         if (count1 < h && citations[j] >= h) {
-        //             count1++;
-        //         }
-        //         else if (count1 >= h && citations[j] <= h) {
-        //             count2++;
-        //         }
-        //     }
-        //     if(count1 + count2 == citations.length) {
-        //         return h;
-        //     }
-        // }
-        
+        return len;
     }
 }
--- 프로그래머스/2/42747. H－Index/README.md
+++ 프로그래머스/2/42747. H－Index/README.md
@@ -1,10 +1,10 @@
 # [level 2] H-Index - 42747 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747?language=java) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.18 ms
+메모리: 73.1 MB, 시간: 0.92 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 31일 13:16:42
+2025년 06월 17일 16:24:52
 
 ### 문제 설명
 
