5d54502067e813f6f2525d8ad0275fdddbb6459c
gomin0 <gomin0981113@naver.com>
2025-06-23 16:15:02 +0900

    [level 2] Title: 전화번호 목록, Time: 335.29 ms, Memory: 97.4 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=java) 
 
 ### 성능 요약
 
-메모리: 30.4 MB, 시간: 102.58 ms
+메모리: 97.4 MB, 시간: 335.29 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 04일 10:52:26
+2025년 06월 23일 16:14:58
 
 ### 문제 설명
 
--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,36 +1,13 @@
 import java.util.*;
+
 class Solution {
     public boolean solution(String[] phone_book) {
-        // for(int i = 0; i < phone_book.length; i++) {
-        //     for(int j = i + 1; j < phone_book.length; j++) {
-        //         if (phone_book[i].startsWith(phone_book[j])) {
-        //             return false;
-        //         }
-        //         if (phone_book[j].startsWith(phone_book[i])) {
-        //             return false;
-        //         }
-        //     }
-        // }
-        
+        boolean answer = true;
         Arrays.sort(phone_book);
-        for(int i = 0; i < phone_book.length - 1; i++) {
-            if (phone_book[i + 1].startsWith(phone_book[i])) return false;
+        for (int i = 0; i < phone_book.length-1; i++) {
+            if (phone_book[i+1].startsWith(phone_book[i]))
+                return false;
         }
         return true;
     }
-//         Map<String, Integer> map = new HashMap<>();
-        
-//         for (int i = 0; i < phone_book.length; i++) {
-//             map.put(phone_book[i], i);
-//         }
-        
-//         for (int i = 0; i < phone_book.length; i++) {
-//             for (int j = 0; j < phone_book[i].length(); j++) {
-//                 if (map.containsKey(phone_book[i].substring(0,j))) {
-//                     return false;
-//                 }
-//             }
-//         }
-//         return true;
-//     }
 }
