e4589b2185f9f09a52c15a1a7323c05c8aa537ee
gomin0 <gomin0981113@naver.com>
2025-06-23 10:59:30 +0900

    [level 3] Title: 징검다리 건너기, Time: 417.64 ms, Memory: 17.6 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.py
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.py
@@ -1,52 +1,25 @@
-# deque 활용 O(n)
-# 모든 k길이 구간별 최댓값 중 최소값 구하기
-from collections import deque
-
 def solution(stones, k):
-    n = len(stones)
-    queue = deque()
-    answer = float('inf')
+    answer: int = 0
+    left: int = 0
+    right: int = max(stones)
     
-    for i in range(n):
-        # 뒤에서부터 현재 값보다 작은 값을 제거
-        while queue and queue[-1][1] < stones[i]:
-            queue.pop()
-        
-        queue.append((i, stones[i]))
-        
-        if queue[0][0] <= i - k:
-            queue.popleft()
-            
-        if i >= k - 1:
-            answer = min(answer, queue[0][1])
-        # print(i, queue[0][1])
-            
+    while left <= right:
+        mid: int = (left + right) // 2
+        count: int = 0
+        possible: bool = True
+        for stone in stones:
+            if stone < mid:
+                count += 1
+                if count >= k:
+                    possible = False
+                    break
+            else:
+                count = 0
+                
+        if possible:
+            answer = mid
+            left = mid + 1
+        else:
+            right = mid - 1
+    
     return answer
-
-
-
-# # 이분 탐색
-# def solution(stones, k):
-#     left = 0
-#     right = max(stones)
-    
-#     while left <= right:
-#         mid = (left + right) // 2
-#         possible = True
-        
-#         count = 0
-#         for stone in stones:
-#             if stone < mid:
-#                 count += 1
-#                 if count >= k:
-#                     possible = False
-#                     break
-#             else:
-#                 count = 0    
-        
-#         if possible:
-#             left = mid + 1
-#         else:
-#             right = mid - 1
-        
-#     return mid
