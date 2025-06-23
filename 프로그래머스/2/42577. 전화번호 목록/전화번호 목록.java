84bbc356c02ca6989122cea2843860827014ee0d
gomin0 <gomin0981113@naver.com>
2025-06-23 16:48:17 +0900

    [level 2] Title: 전화번호 목록, Time: 281.90 ms, Memory: 247 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,11 +2,17 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        boolean answer = true;
-        Arrays.sort(phone_book);
-        for (int i = 0; i < phone_book.length-1; i++) {
-            if (phone_book[i+1].startsWith(phone_book[i]))
-                return false;
+        Set<String> set = new HashSet<>();
+        for (String number : phone_book) {
+            set.add(number);
+        }
+        for (String number : phone_book) {
+            for (int i = 1; i < number.length(); i++) {
+                String prefix = number.substring(0, i);
+                if (set.contains(prefix)) {
+                    return false;
+                }
+            }
         }
         return true;
     }
