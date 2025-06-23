c589bad37fab4f3573d902f2648465fdeb013c19
gomin0 <gomin0981113@naver.com>
2025-06-23 13:36:17 +0900

    [level 2] Title: 튜플, Time: 24.49 ms, Memory: 11.1 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/README.md
+++ 프로그래머스/2/64065. 튜플/README.md
@@ -1,10 +1,10 @@
 # [level 2] 튜플 - 64065 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64065) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64065?language=python3) 
 
 ### 성능 요약
 
-메모리: 11.4 MB, 시간: 126.16 ms
+메모리: 11.1 MB, 시간: 24.49 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 02일 13:47:37
+2025년 06월 23일 13:36:12
 
 ### 문제 설명
 
--- 프로그래머스/2/64065. 튜플/튜플.py
+++ 프로그래머스/2/64065. 튜플/튜플.py
@@ -1,20 +1,16 @@
 def solution(s):
-    answer = []
-    # 많이 나온 순으로 배열에 담기면 되겠다
-    num_count = {}
+    answer: list[int] = []
+    s = s[2:-2]
+    tuple_set: list[str] = s.split("},{")
+    result_set: set[int] = set()
+    tuple_set.sort(key=len)
     
-    num = ''
-    for i in range(1, len(s)):
-        if s[i].isdigit():
-            num += s[i]
-        elif s[i] == ',' or s[i] == '}':
-            if s[i - 1].isdigit():
-                if num in num_count:
-                    num_count[num] += 1
-                else:
-                    num_count[num] = 1
-            num = ''
-            
-    answer = [int(k) for k, v in sorted(num_count.items(), key=lambda item: item[1], reverse=True)]
+    for ts in tuple_set:
+        numbers: list[str] = ts.split(",")
+        for number in numbers:
+            num: int = int(number)
+            if num not in result_set:
+                result_set.add(num)
+                answer.append(num)
     
     return answer
