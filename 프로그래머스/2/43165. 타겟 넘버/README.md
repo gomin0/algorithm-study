030e4bffcaf164ba5e23fbf80a92a6d6962e31f5
gomin0 <gomin0981113@naver.com>
2025-06-28 10:44:13 +0900

    [level 2] Title: 타겟 넘버, Time: 4.42 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=java) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 443.19 ms
+메모리: 76.8 MB, 시간: 4.42 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 05일 09:57:48
+2025년 06월 28일 10:44:08
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,22 +1,20 @@
 class Solution {
     static int answer = 0;
     public int solution(int[] numbers, int target) {
-        dfs(0, numbers, target, 0);
+        dfs(0, numbers, target, 0);        
         return answer;
     }
     
-    static void dfs(int depth, int[] numbers, int target, int result) {
-        if (depth == numbers.length) {
-            if (target == result) {
+    private void dfs(int depth, int[] numbers, int target, int num) {
+        if (depth == numbers.length){
+            if (num == target)
                 answer++;
-            }
             return;
         }
+        int plus = num + numbers[depth];
+        int minus = num - numbers[depth];
         
-        int plus = result + numbers[depth];
-        int minus = result - numbers[depth];
-        
-        dfs (depth + 1, numbers, target, plus);
-        dfs (depth + 1, numbers, target, minus);
+        dfs(depth + 1, numbers, target, plus);
+        dfs(depth + 1, numbers, target, minus);
     }
 }
