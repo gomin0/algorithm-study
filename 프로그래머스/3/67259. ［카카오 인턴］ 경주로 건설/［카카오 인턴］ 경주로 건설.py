69e117d11ef690a1409bd1f73625ff8612e06467
gomin0 <gomin0981113@naver.com>
2025-07-01 11:48:45 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 11.44 ms, Memory: 9.33 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
@@ -2,31 +2,30 @@ import heapq
 
 def solution(board):
     answer = 0
-    
-    n = len(board)
-    
-    prices = [[[float('inf')] * 4 for _ in range(n)] for _ in range(n)]
-    direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-    
-    queue = []
+    dx: list[int] = [-1, 1, 0, 0]
+    dy: list[int] = [0, 0, -1, 1]
+    INF: float = float('inf')
+    n: int = len(board)
+    costs = [[[INF] * 4 for _ in range(n)] for _ in range(n)]
+    pq = []
     
     for i in range(4):
-        prices[0][0][i] = 0
-        heapq.heappush(queue, (0, 0, 0, i)) # 가격, x, y, 방향
+        costs[0][0][i] = 0
+        heapq.heappush(pq, (0, 0, 0, i))  # 가격, x, y, 방향
     
-    while queue:
-        price, x, y, d = heapq.heappop(queue)
+    while pq:
+        cost, x, y, direction = heapq.heappop(pq)
         
         if x == n-1 and y == n-1:
-            return price
+            return cost
         
         for i in range(4):
-            dx, dy = direction[i]
-            nx, ny = x + dx, y + dy
-            if 0 <= nx < n and 0 <= ny < n and board[nx][ny] != 1:
-                next_price = price + 100
-                if d != i:
-                    next_price += 500
-                if next_price < prices[nx][ny][i]:
-                    prices[nx][ny][i] = next_price
-                    heapq.heappush(queue, (next_price, nx, ny, i))
+            nx: int = x + dx[i]
+            ny: int = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < n and board[nx][ny] == 0:
+                next_cost: int = cost + 100
+                if direction != i:
+                    next_cost += 500
+                if next_cost < costs[nx][ny][i]:
+                    costs[nx][ny][i] = next_cost
+                    heapq.heappush(pq, (next_cost, nx, ny, i))
