5f9ee1786a8d54d8cf46b1867991ffe2f6e533f0
gomin0 <gomin0981113@naver.com>
2025-07-01 15:52:00 +0900

    [level 2] Title: 모음 사전, Time: 0.21 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/README.md
+++ 프로그래머스/2/84512. 모음 사전/README.md
@@ -1,10 +1,10 @@
 # [level 2] 모음 사전 - 84512 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512?language=java) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 2.47 ms
+메모리: 78.1 MB, 시간: 0.21 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 06일 10:42:26
+2025년 07월 01일 15:51:55
 
 ### 문제 설명
 
--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,27 +1,21 @@
 import java.util.*;
+
 class Solution {
-    static List<String> list;
-    static String[] words = {"A", "E", "I", "O", "U"};
     public int solution(String word) {
+        int[] weights = {781, 156, 31, 6, 1};
+        Map <Character, Integer> wordDict = new HashMap<>();
+        wordDict.put('A', 0);
+        wordDict.put('E', 1);
+        wordDict.put('I', 2);
+        wordDict.put('O', 3);
+        wordDict.put('U', 4);
         int answer = 0;
-        list = new ArrayList<>();
-        dfs("", 0);
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i).equals(word)) {
-                answer = i;
-                break;
-            }
+        
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            answer += wordDict.get(c) * weights[i] + 1;
         }
+        
         return answer;
-    }
-    
-    static void dfs(String str, int len) {
-        list.add(str);
-        if(len == 5) {
-            return;
-        }
-        for (int i = 0; i < 5; i++) {
-            dfs(str + words[i], len + 1);
-        }
     }
 }
