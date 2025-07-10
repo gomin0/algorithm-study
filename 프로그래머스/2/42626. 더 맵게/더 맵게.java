7fbbc61e22ddc06bfc797d71edc7b7d1d21fa8c7
gomin0 <gomin0981113@naver.com>
2025-07-10 17:48:51 +0900

    [level 2] Title: 더 맵게, Time: 1468.14 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,25 +1,28 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int[] scoville, int K) {        
+    public int solution(int[] scoville, int K) {
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         int answer = 0;
         
-        for(int i : scoville) {
-            pq.add(i);
-        }
+        for (int s : scoville)
+            pq.add(s);
         
-        while (pq.peek() < K) {
-            if (pq.size() < 2) {
-                return -1;
+        while (true) {
+            if (!pq.isEmpty()) {
+                int minScoville = pq.poll();
+                if (minScoville >= K)
+                    return answer;
+                else if (pq.size() >= 1) {
+                    int minScoville2 = pq.poll();
+                    pq.add(minScoville + minScoville2 * 2);
+                    answer++;
+                }
+                else
+                    return -1;
             }
-            int a = pq.poll();
-            int b = pq.poll();
-            int mix = a + (b*2);
-            pq.add(mix);
-            answer++;
+            else
+                return -1;
         }
-        
-        return answer;
     }
 }
