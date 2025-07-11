da59c49aec86b1cf5ae0239a4e3ce638cf26bd14
gomin0 <gomin0981113@naver.com>
2025-07-11 10:48:42 +0900

    [level 2] Title: 주식가격, Time: 43.69 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,22 +1,25 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] prices) {
         int len = prices.length;
         int[] answer = new int[len];
+        Stack<Integer> stack = new Stack<>();
         
         for (int i=0; i<len; i++) {
-            int price = prices[i];
-            int count = -1;
-            for (int j=i; j<len; j++) {
-                int nPrice = prices[j];
-                if (price <= nPrice)
-                    count++;
-                else {
-                    count++;
-                    break;
-                }
+            while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
+                int idx = stack.pop();
+                answer[idx] = i - idx;
             }
-            answer[i] = count;
+            stack.push(i);
         }
+        
+        // 끝까지 가격이 떨어지지 않은 경우
+        while (!stack.isEmpty()) {
+            int idx = stack.pop();
+            answer[idx] = len - idx - 1;
+        }
+        
         return answer;
     }
 }
