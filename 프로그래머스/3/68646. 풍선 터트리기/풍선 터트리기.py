7da19b095c4979f6a12be014f5c59a9a7dcc1365
gomin0 <gomin0981113@naver.com>
2025-07-12 11:43:12 +0900

    [level 3] Title: 풍선 터트리기, Time: 749.09 ms, Memory: 62.9 MB -BaekjoonHub

--- 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.py
+++ 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.py
@@ -1,21 +1,22 @@
-def solution(a):
-    answer = 2
+def solution(a) -> int:
+    answer: int = 0
+    length: int = len(a)
+    min_left: list[int] = [0] * length
+    min_right: list[int] = [0] * length
     
-    n = len(a)
+    min_left[0] = a[0]
+    for i in range(1, length):
+        min_left[i] = min(min_left[i-1], a[i])
+    min_right[-1] = a[-1]
+    for i in range(length-2, -1, -1):
+        min_right[i] = min(min_right[i+1], a[i])
     
-    left_min = [0 for _ in range(n)]
-    right_min = [0 for _ in range(n)]
-    
-    left_min[0] = a[0]
-    for i in range(1, n):
-        left_min[i] = min(left_min[i-1], a[i])
-    
-    right_min[n-1] = a[n-1]
-    for i in range(n-2, -1, -1):
-        right_min[i] = min(right_min[i+1], a[i])
-    
-    for i in range(1, n-1):
-        if not(a[i] > left_min[i] and a[i] > right_min[i]):
+    for i in range(length):
+        if i == 0 or i == length-1:
             answer += 1
+            continue
+        if min_left[i-1] < a[i] and min_right[i+1] < a[i]:
+            continue
+        answer += 1
     
     return answer
