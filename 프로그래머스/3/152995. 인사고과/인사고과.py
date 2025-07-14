93a847f7f728122589dbd925e326a44eacdcbb77
gomin0 <gomin0981113@naver.com>
2025-07-14 11:09:13 +0900

    [level 3] Title: 인사고과, Time: 181.40 ms, Memory: 34.9 MB -BaekjoonHub

--- 프로그래머스/3/152995. 인사고과/인사고과.py
+++ 프로그래머스/3/152995. 인사고과/인사고과.py
@@ -1,18 +1,14 @@
 def solution(scores):
-    answer = 1
-    
-    wanho = scores[0]
-    wanho_sum = sum(scores[0])
-    scores = sorted(scores, key=lambda x: (-x[0], x[1]))
-    
-    max_b = 0
+    wanho: list[int] = scores[0]
+    scores.sort(key=lambda x: (-x[0], x[1]))
+    answer: int = 1
+    max_b: int = 0
     for a, b in scores:
         if wanho[0] < a and wanho[1] < b:
             return -1
-        
         if b >= max_b:
             max_b = b
-            if a+b > wanho_sum:
+            if a+b > sum(wanho):
                 answer += 1
-        
+    
     return answer
