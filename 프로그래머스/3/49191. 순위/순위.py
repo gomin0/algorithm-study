9b9591062153e8d02f8f9597e92a4e50681c5e46
gomin0 <gomin0981113@naver.com>
2025-07-14 09:42:23 +0900

    [level 3] Title: 순위, Time: 95.24 ms, Memory: 9.43 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.py
+++ 프로그래머스/3/49191. 순위/순위.py
@@ -1,24 +1,22 @@
 def solution(n, results):
-    answer = 0
-    
-    player = [[False for _ in range(n)] for _ in range(n)]
+    graph: list[list[False]] = [[False] * n for _ in range(n)]
     
     for i in range(n):
-        player[i][i] = True
-    
+        graph[i][i] = True
     for a, b in results:
-        player[a-1][b-1] = True
-        
+        graph[a-1][b-1] = True
+    
     for k in range(n):
         for i in range(n):
             for j in range(n):
-                if player[i][k] and player[k][j]:
-                    player[i][j] = True
+                if graph[i][k] and graph[k][j]:
+                    graph[i][j] = True
     
+    answer: int = 0
     for i in range(n):
         count = 0
         for j in range(n):
-            if player[i][j] or player[j][i]:
+            if graph[i][j] or graph[j][i]:
                 count += 1
         if count == n:
             answer += 1
