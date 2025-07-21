0234f80a2204b869af01c5af96728394a5ae5e13
gomin0 <gomin0981113@naver.com>
2025-07-21 11:57:38 +0900

    [level 2] Title: 오픈채팅방, Time: 146.22 ms, Memory: 152 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/README.md
+++ 프로그래머스/2/42888. 오픈채팅방/README.md
@@ -1,10 +1,10 @@
 # [level 2] 오픈채팅방 - 42888 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42888?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42888) 
 
 ### 성능 요약
 
-메모리: 180 MB, 시간: 138.91 ms
+메모리: 152 MB, 시간: 146.22 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 28일 10:24:08
+2025년 07월 21일 11:57:34
 
 ### 문제 설명
 
--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -1,31 +1,35 @@
 import java.util.*;
 
 class Solution {
-    public ArrayList<String> solution(String[] record) {        
-        Map<String, String> name = new HashMap<>();
-        ArrayList<String> answer = new ArrayList<>();
-
-        for (String r : record) {
-            String[] parts = r.split(" ");
-            String command = parts[0];
-            String uid = parts[1];
-            if (!command.equals("Leave")) {
-                String nickname = parts[2];
-                name.put(uid, nickname);
-            }
-        }
+    public String[] solution(String[] record) {
+        Map<String, String> user = new HashMap<>();
+        List<String> action = new ArrayList<>();
+        List<String> userId = new ArrayList<>();
         
         for (String r : record) {
             String[] parts = r.split(" ");
-            String command = parts[0];
+            String act = parts[0];
             String uid = parts[1];
-            String nickname = name.get(uid);
-            
-            if (command.equals("Enter")) {
-                answer.add(nickname + "님이 들어왔습니다.");
-            } else if (command.equals("Leave")) {
-                answer.add(nickname + "님이 나갔습니다.");
+            if (!act.equals("Change")) {
+                action.add(act);
+                userId.add(uid);
             }
+            if (parts.length == 3) {
+                String name = parts[2];
+                user.put(uid, name);
+            }
+        }
+        
+        int size = action.size();
+        String[] answer = new String[size];
+        for (int i = 0; i < size; i++) {
+            String userName = user.get(userId.get(i));
+            String result = userName + "님이 ";
+            if (action.get(i).equals("Enter"))
+                result += "들어왔습니다.";
+            else
+                result += "나갔습니다.";
+            answer[i] = result;
         }
         
         return answer;
