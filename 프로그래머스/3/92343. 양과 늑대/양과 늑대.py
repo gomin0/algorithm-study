c0ca941d145cef912b8e6d2acf9d7b1102733b3c
gomin0 <gomin0981113@naver.com>
2025-07-21 10:30:14 +0900

    [level 3] Title: 양과 늑대, Time: 11.16 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
@@ -1,26 +1,31 @@
 from collections import defaultdict
 
 def solution(info, edges):
-    answer = 0
-    
-    tree = defaultdict(list)
+    answer: int = 0
+    tree: defaultdict[int, list[int]] = defaultdict(list)
     for parent, child in edges:
         tree[parent].append(child)
-    
+        
     def dfs(node, sheep, wolf, next_nodes):
         if info[node] == 0:
             sheep += 1
         else:
             wolf += 1
-        if sheep <= wolf:
+        if wolf >= sheep:
             return sheep
         max_sheep = sheep
         
-        for n in tree[node]:
-            next_nodes.append(n)
         for i, next_node in enumerate(next_nodes):
-            max_sheep = max(max_sheep, dfs(next_node, sheep, wolf, next_nodes[:i] + next_nodes[i+1:]))
-    
+            max_sheep = max(
+                max_sheep,
+                dfs(
+                    next_node,
+                    sheep,
+                    wolf,
+                    next_nodes[:i] + next_nodes[i+1:] + tree[next_node]
+                )
+            )
+        
         return max_sheep
-            
-    return dfs(0, 0, 0, [])
+    
+    return dfs(0, 0, 0, tree[0])
