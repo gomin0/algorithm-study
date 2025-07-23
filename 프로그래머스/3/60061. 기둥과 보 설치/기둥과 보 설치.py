f50c80f8a2a0eb638699b0db0b457e04cd468426
gomin0 <gomin0981113@naver.com>
2025-07-23 11:11:06 +0900

    [level 3] Title: 기둥과 보 설치, Time: 49.34 ms, Memory: 9.52 MB -BaekjoonHub

--- 프로그래머스/3/60061. 기둥과 보 설치/기둥과 보 설치.py
+++ 프로그래머스/3/60061. 기둥과 보 설치/기둥과 보 설치.py
@@ -1,28 +1,30 @@
-def check(structures):  # 현재 구조물이 유효한지 체크
-    for x, y, a in structures:
-        if a == 0:  #기둥
-            if y == 0 or (x, y-1, 0) in structures or (x-1, y, 1) in structures or (x,y, 1) in structures:  # 바닥 위 or 다른 기둥 위 or 왼쪽 보 위 or 오른쪽 보 위
-                continue  # 통과
-            return False  # 유효하지 않음
-        elif a == 1:  # 보
-            if (x, y-1, 0) in structures or (x+1, y-1, 0) in structures or ((x-1, y, 1) in structures and (x+1, y, 1) in structures):  # 왼쪽 기둥 위 or 오른쪽 기둥 위 or 양쪽이 보로 연결
-                continue  # 통과
-            return False  # 유효하지 않음
-    return True  # 유효한 경우
-    
+def check(struct):
+    for x, y, a in struct:
+        if a == 0:
+            if y == 0 or (x, y-1, 0) in struct or (x-1, y, 1) in struct or (x,y, 1) in struct:
+                continue
+            return False
+        else:
+            if (x, y-1, 0) in struct or (x+1, y-1, 0) in struct or ((x-1, y, 1) in struct and (x+1, y, 1) in struct):
+                continue
+            return False
+    return True
+
 def solution(n, build_frame):
-    structures = set()
+    struct: set[tuple[int, int ,int]] = set()
     
     for x, y, a, b in build_frame:
-        if b == 0:  # 삭제
-            structures.remove((x, y, a))  # 삭제
-            if not check(structures):  # 유효하지 않으면
-                structures.add((x, y, a))  # 복구
-        else:  # 설치
-            structures.add((x, y, a))  # 설치
-            if not check(structures):  # 유효하지 않으면
-                structures.remove((x, y, a))  # 삭제
-        
-        answer = sorted(structures, key=lambda item: (item[0], item[1], item[2]))
-        
-    return answer
+        if b == 0:
+            struct.remove((x, y, a))
+            if not check(struct):
+                struct.add((x, y, a))
+        else:
+            struct.add((x, y, a))
+            if not check(struct):
+                struct.remove((x, y, a))
+    
+    return sorted(struct, key=lambda item: (item[0], item[1], item[2]))
+
+# 가로좌표, 세로좌표, (0기둥, 1 보), (0 삭제, 1 설치)
+# 기둥 -> 바닥 위 or 보의 한쪽 끝 위 or 다른 기둥 위
+# 보 -> 한쪽 끝이 기둥 위 or 양 쪽 끝이 다른 보와 연결
