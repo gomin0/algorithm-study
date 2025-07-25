eab03f455a368fb202fc3ae28be647c74a9f9ac0
gomin0 <gomin0981113@naver.com>
2025-07-25 15:39:10 +0900

    [level 3] Title: 110 옮기기, Time: 358.19 ms, Memory: 25 MB -BaekjoonHub

--- 프로그래머스/3/77886. 110 옮기기/110 옮기기.py
+++ 프로그래머스/3/77886. 110 옮기기/110 옮기기.py
@@ -1,34 +1,27 @@
 def solution(s):
     answer = []
-    
+
     for string in s:
-        count110 = 0
         stack = []
-        
-        # 문자열을 한 번 순회하면서 "110"을 제거하고 그 개수를 센다
-        i = 0
-        while i < len(string):
-            if string[i] == '0':
-                # stack의 끝에 "110"이 있다면 제거하고 count를 증가시킵니다.
-                if len(stack) >= 2 and stack[-2] == '1' and stack[-1] == '1':
-                    stack.pop()
-                    stack.pop() # 11 제거 -> 110 빼기
-                    count110 += 1
-                    i += 1
-                else:
-                    stack.append(string[i])
-                    i += 1
-            else:
-                stack.append(string[i])
-                i += 1
-                
-        stack = ''.join(stack)
-        idx = stack.rfind('0')
-        # "110" 삽입
-        if idx != -1:  # find함수는 없으면 -1 반환
-            new_string = stack[:idx+1] + '110' * count110 + stack[idx+1:]
+        count = 0
+
+        for c in string:
+            stack.append(c)
+            if len(stack) >= 3 and stack[-3:] == ['1', '1', '0']:
+                stack.pop()
+                stack.pop()
+                stack.pop()
+                count += 1
+
+        idx = len(stack)
+        for i in range(len(stack) - 1, -1, -1):
+            if stack[i] == '0':
+                idx = i + 1
+                break
         else:
-            new_string = '110' * count110 + stack
-        answer.append(new_string)
-    
+            idx = 0
+
+        result = stack[:idx] + ['110'] * count + stack[idx:]
+        answer.append(''.join(result))
+
     return answer
