769e5fa4642dbda6a85d7ddb4b9af3e83ee5ff7c
gomin0 <gomin0981113@naver.com>
2025-07-27 13:08:42 +0900

    [level 3] Title: 외벽 점검, Time: 1172.97 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/3/60062. 외벽 점검/외벽 점검.py
+++ 프로그래머스/3/60062. 외벽 점검/외벽 점검.py
@@ -1,22 +1,22 @@
 from itertools import permutations
 
 def solution(n, weak, dist):
+    length: int = len(weak)
+    weak = weak + [i + n for i in weak]
     answer = float('inf')
     
-    length = len(weak)
-    extended_weak = weak + [w + n for w in weak]
-    
-    for i, start in enumerate(weak):
-        for friends in permutations(dist):
+    for friends in permutations(dist):
+        for start in range(length):
             count = 1
-            position = start
+            position = weak[start] + friends[count - 1]
             
-            for friend in friends:
-                position += friend
-                if position < extended_weak[i+length - 1]: # 마지막에 도달 못함
+            for i in range(start, start + length):
+                if weak[i] > position:
                     count += 1
-                    position = [w for w in extended_weak[i+1:i+length] if w > position][0]
-                else:
-                    answer = min(answer, count)
-                    break
+                    if count > len(friends):
+                        break
+                    position = weak[i] + friends[count - 1]
+            else:
+                answer = min(answer, count)
+    
     return answer if answer != float('inf') else -1
