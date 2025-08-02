dd42a096ca7e54a8d46a16ae077b355c79adf960
gomin0 <gomin0981113@naver.com>
2025-08-02 10:36:02 +0900

    [level 4] Title: 올바른 괄호의 갯수, Time: 2139.36 ms, Memory: 9.09 MB -BaekjoonHub

--- 프로그래머스/4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.py
+++ 프로그래머스/4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.py
@@ -1,15 +1,14 @@
 def solution(n):
-    count = 0
-
-    def dfs(open_cnt, close_cnt):
-        nonlocal count
-        if open_cnt == n and close_cnt == n:
-            count += 1
-            return
-        if open_cnt < n:
-            dfs(open_cnt + 1, close_cnt)
-        if close_cnt < open_cnt:
-            dfs(open_cnt, close_cnt + 1)
-
+    answer = 0
+    
+    def dfs(open, close):
+        nonlocal answer
+        if open == n and close == n:
+            answer += 1
+        if open < n:
+            dfs(open+1, close)
+        if close < open:
+            dfs(open, close+1)
+    
     dfs(0, 0)
-    return count
+    return answer
