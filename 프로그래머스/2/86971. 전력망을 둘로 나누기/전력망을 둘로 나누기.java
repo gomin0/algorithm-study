067423d06d4117aba1f5a721f82e2b989d3f06d4
gomin0 <gomin0981113@naver.com>
2025-08-03 13:33:11 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 14.25 ms, Memory: 96.7 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -4,41 +4,36 @@ class Solution {
     public int solution(int n, int[][] wires) {
         int answer = n;
         int[][] graph = new int[n+1][n+1];
-        
         for (int i = 0; i < wires.length; i++) {
             int a = wires[i][0];
             int b = wires[i][1];
             graph[a][b] = 1;
             graph[b][a] = 1;
         }
-        
         for (int i = 0; i < wires.length; i++) {
             int a = wires[i][0];
             int b = wires[i][1];
             graph[a][b] = 0;
             graph[b][a] = 0;
-            
             answer = Math.min(answer, bfs(n, a, graph));
             graph[a][b] = 1;
             graph[b][a] = 1;
         }
-        
         return answer;
     }
     
-    private int bfs(int n, int a, int[][]graph) {
+    private int bfs(int n, int a, int[][] graph) {
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(a);
         boolean[] visited = new boolean[n+1];
         visited[a] = true;
         int count = 1;
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(a);
-        
-        while (!queue.isEmpty()) {
-            int node = queue.poll();
+        while (!q.isEmpty()) {
+            int node = q.poll();
             for (int i = 1; i <= n; i++) {
-                if (graph[node][i] == 1 && !visited[i]) {
+                if(graph[node][i] == 1 && !visited[i]) {
                     visited[i] = true;
-                    queue.offer(i);
+                    q.offer(i);
                     count++;
                 }
             }
