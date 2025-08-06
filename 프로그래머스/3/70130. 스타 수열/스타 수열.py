214140b922873d8eb3cabab8f21998c9aca0cb83
gomin0 <gomin0981113@naver.com>
2025-08-06 09:34:48 +0900

    [level 3] Title: 스타 수열, Time: 572.58 ms, Memory: 45.7 MB -BaekjoonHub

--- 프로그래머스/3/70130. 스타 수열/스타 수열.py
+++ 프로그래머스/3/70130. 스타 수열/스타 수열.py
@@ -1,26 +1,25 @@
 from collections import Counter
 
 def solution(a):
-    answer = 0
-    
-    if len(a) < 2:
+    n: int = len(a)
+    if n == 1:
         return 0
+    if n == 2:
+        return 2
     
+    answer: int = 0
     counter = Counter(a)
-    
-    for key in counter.keys():
-        # 현재 숫자의 등장 횟수가 현재까지의 최대 길이의 절반보다 작다면 건너뜀
+    for key in counter:
         if counter[key] * 2 <= answer:
             continue
-            
-        count = 0
-        i = 0
-        while i < len(a) - 1:
+        
+        count: int = 0
+        i: int =  0
+        while i < n - 1:
             if (a[i] == key or a[i+1] == key) and a[i] != a[i+1]:
                 count += 2
                 i += 1
             i += 1
-            
         answer = max(answer, count)
     
     return answer
