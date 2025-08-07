7c5857a1e7362d3cb966b64c714f7a304617a4e9
gomin0 <gomin0981113@naver.com>
2025-08-07 11:00:03 +0900

    [level 3] Title: 등산코스 정하기, Time: 604.14 ms, Memory: 89.8 MB -BaekjoonHub

--- 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.py
+++ 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.py
@@ -4,33 +4,31 @@ from collections import defaultdict
 def solution(n, paths, gates, summits):
     graph = defaultdict(list)
     for n1, n2, d in paths:
-        graph[n1].append((n2, d))
-        graph[n2].append((n1, d))
+        graph[n1].append((d, n2))
+        graph[n2].append((d, n1))
     
-    # in 빠르게 하기 위함
     summits_set = set(summits)
     intensity = [float('inf')] * (n + 1)
     hq = []
-    
     for gate in gates:
         heapq.heappush(hq, (0, gate))
         intensity[gate] = 0
     
     while hq:
         dist, node = heapq.heappop(hq)
-        if node in summits_set or dist > intensity[node]:
+        if node in summits_set or intensity[node] < dist:
             continue
-        for next_node, next_dist in graph[node]:
+        for next_dist, next_node in graph[node]:
             new_intensity = max(dist, next_dist)
-            if new_intensity < intensity[next_node]:
+            if intensity[next_node] > new_intensity:
                 intensity[next_node] = new_intensity
                 heapq.heappush(hq, (new_intensity, next_node))
     
-    min_intensity = float('inf')
     min_summit = 0
+    min_intensity = float('inf')
     for summit in sorted(summits):
-        if intensity[summit] < min_intensity:
+        if min_intensity > intensity[summit]:
             min_intensity = intensity[summit]
             min_summit = summit
-            
+    
     return [min_summit, min_intensity]
