ee617c005e90cc4222bf68d8d0b4e8d3dede8bc1
gomin0 <gomin0981113@naver.com>
2025-08-08 15:55:41 +0900

    [level 2] Title: 배달, Time: 2.43 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.py
+++ 프로그래머스/2/12978. 배달/배달.py
@@ -1,70 +1,36 @@
-# 플로이드 워셜
-def floyd_warshall(graph, N):
-    for k in range(1, N+1):
-        for i in range(1, N+1):
-            for j in range(1, N+1):
-                graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
-    return graph
-    
-
-def solution1(N, road, K):
-    answer = 0
-
-    graph = [[float('inf')] * (N+1) for _ in range(N+1)]
-    
-    for i in range(1, N+1):
-        graph[i][i] = 0
-        
-    for start, end, time in road:
-        graph[start][end] = min(graph[start][end], time)
-        graph[end][start] = min(graph[end][start], time)
-    
-    graph = floyd_warshall(graph, N)
-    
-    for j in range(1, N + 1):
-        if graph[1][j] <= K:
-            answer += 1
-
-    return answer
-
-
-# 다익스트라
 import heapq
 from collections import defaultdict
 
-def dijkstra(start, N, graph):
-    
-    distance = {i: float('inf') for i in range(1, N + 1)}
+def dijkstra(graph, N, start):
+    distance = {i: float('inf') for i in range(1, N+1)}
     distance[start] = 0
     
-    queue = []
-    heapq.heappush(queue, (0, start))
-    
-    while queue:
-        now_distance, now_node = heapq.heappop(queue)
-        if now_distance > distance[now_node]:
+    hq = []
+    heapq.heappush(hq, (0, start))
+    while hq:
+        dist, node = heapq.heappop(hq)
+        if dist > distance[node]:
             continue
-        
-        for next_node, next_distance in graph[now_node]:
-            dist = now_distance + next_distance
-            if dist < distance[next_node]:
-                distance[next_node] = dist
-                heapq.heappush(queue, (dist, next_node))
-    
+        for next_dist, next_node in graph[node]:
+            new_dist = dist + next_dist
+            if distance[next_node] > new_dist:
+                distance[next_node] = new_dist
+                heapq.heappush(hq, (new_dist, next_node))
     return distance
+    
 
 def solution(N, road, K):
     answer = 0
-
-    graph = defaultdict(list)
-    for start, end, time in road:
-        graph[start].append((end, time))
-        graph[end].append((start, time))
-        
-    distance = dijkstra(1, N, graph)
     
-    for i in distance.values():
-        if i <= K:
+    graph = defaultdict(list)
+    for n1, n2, d in road:
+        graph[n1].append((d, n2))
+        graph[n2].append((d, n1))
+    
+    distance = dijkstra(graph, N, 1)
+    
+    for i in range(1, N+1):
+        if distance[i] <= K:
             answer += 1
-
+    
     return answer
