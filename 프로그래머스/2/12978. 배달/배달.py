3fe3d47dea6c291874fa278d8b686c2f4dd5b39a
gomin0 <gomin0981113@naver.com>
2025-08-09 10:06:15 +0900

    [level 2] Title: 배달, Time: 42.44 ms, Memory: 9.64 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.py
+++ 프로그래머스/2/12978. 배달/배달.py
@@ -1,68 +1,27 @@
-# 플로이드 워셜
-def floyd_warshall(graph, N):
+def floyd(N, graph):
     for k in range(1, N+1):
         for i in range(1, N+1):
             for j in range(1, N+1):
                 graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
     return graph
-    
+
 
 def solution(N, road, K):
     answer = 0
-
+    
     graph = [[float('inf')] * (N+1) for _ in range(N+1)]
     
     for i in range(1, N+1):
         graph[i][i] = 0
-        
+    
     for start, end, time in road:
         graph[start][end] = min(graph[start][end], time)
         graph[end][start] = min(graph[end][start], time)
     
-    graph = floyd_warshall(graph, N)
+    graph = floyd(N, graph)
     
-    for j in range(1, N + 1):
-        if graph[1][j] <= K:
-            answer += 1
-
-    return answer
-
-
-# 다익스트라
-import heapq
-from collections import defaultdict
-
-def dijkstra(N, start, graph):
-    distance = {i: float('inf') for i in range(1, N+1)}
-    distance[start] = 0
-    
-    hq = []
-    heapq.heappush(hq, (0, start))
-    
-    while hq:
-        now_dist, now_node = heapq.heappop(hq)
-        if now_dist > distance[now_node]:
-            continue
-        for next_node, next_dist in graph[now_node]:
-            dist = now_dist + next_dist
-            if dist < distance[next_node]:
-                distance[next_node] = dist
-                heapq.heappush(hq, (dist, next_node))
-    
-    return distance
-
-def solution1(N, road, K):
-    answer = 0
-
-    graph = defaultdict(list)
-    for a, b, c in road:
-        graph[a].append((b, c))
-        graph[b].append((a, c))
-    
-    distances = dijkstra(N, 1, graph)
-    
-    for i in distances.values():
-        if i <= K:
+    for i in range(1, N+1):
+        if graph[1][i] <= K:
             answer += 1
     
     return answer
