58a2048a682282d7d2fd2be6753963feb5e99032
gomin0 <gomin0981113@naver.com>
2025-08-11 16:17:53 +0900

    [level 4] Title: 징검다리, Time: 160.40 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.py
+++ 프로그래머스/4/43236. 징검다리/징검다리.py
@@ -1,6 +1,5 @@
 def solution(distance, rocks, n):
     answer = 0
-    
     rocks.sort()
     rocks.append(distance)
     
@@ -9,17 +8,15 @@ def solution(distance, rocks, n):
         mid = (left + right) // 2
         current = 0
         remove = 0
-        
         for rock in rocks:
             if rock - current < mid:
                 remove += 1
             else:
                 current = rock
-        
         if remove <= n:
             answer = mid
             left = mid + 1
         else:
             right = mid - 1
-    
+            
     return answer
