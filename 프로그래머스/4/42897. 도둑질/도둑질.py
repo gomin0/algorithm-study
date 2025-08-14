3847325e8a6de37b5085bb3f00e263d04d5e0841
gomin0 <gomin0981113@naver.com>
2025-08-14 14:12:05 +0900

    [level 4] Title: 도둑질, Time: 606.48 ms, Memory: 91.5 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.py
+++ 프로그래머스/4/42897. 도둑질/도둑질.py
@@ -1,17 +1,14 @@
 def solution(money):
-    n = len(money)
-    
+    n: int = len(money)
+
     dp1 = [0] * n
     dp1[0] = money[0]
     dp1[1] = money[0]
-    
     for i in range(2, n-1):
         dp1[i] = max(dp1[i-1], dp1[i-2] + money[i])
-    
+        
     dp2 = [0] * n
-    dp2[0] = 0
     dp2[1] = money[1]
-    
     for i in range(2, n):
         dp2[i] = max(dp2[i-1], dp2[i-2] + money[i])
     
