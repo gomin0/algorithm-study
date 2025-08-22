ea2ed6b03f2f0007165effaa6ed1e63926d7a919
gomin0 <gomin0981113@naver.com>
2025-08-22 14:55:29 +0900

    [level 3] Title: 등굣길, Time: 0.97 ms, Memory: 54 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/README.md
+++ 프로그래머스/3/42898. 등굣길/README.md
@@ -1,10 +1,10 @@
 # [level 3] 등굣길 - 42898 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42898#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42898?language=java) 
 
 ### 성능 요약
 
-메모리: 9.32 MB, 시간: 5.12 ms
+메모리: 54 MB, 시간: 0.97 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 11일 11:46:51
+2025년 08월 22일 14:55:26
 
 ### 문제 설명
 
--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,27 +1,20 @@
 class Solution {
     public int solution(int m, int n, int[][] puddles) {
         int mod = 1000000007;
-        int[][] dp = new int[m][n];
-        for (int[] puddle : puddles) {
-            dp[puddle[0] - 1][puddle[1] - 1] = -1;
-        }
-        
+        int[][] dp = new int[n][m];
+        for (int i = 0; i < puddles.length; i++)
+            dp[puddles[i][1] - 1][puddles[i][0] - 1] = -1;
         dp[0][0] = 1;
-        
-        for(int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if(dp[i][j] == -1) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (dp[i][j] == -1) {
                     dp[i][j] = 0;
                     continue;
                 }
-                
-                if(i != 0) // 맨위 아님
-                    dp[i][j] += dp[i - 1][j] % mod;
-
-                if(j != 0) // 맨옆 아님
-                    dp[i][j] += dp[i][j - 1] % mod;
+                if (i > 0) dp[i][j] = (dp[i][j] + dp[i-1][j]) % mod;
+                if (j > 0) dp[i][j] = (dp[i][j] + dp[i][j-1]) % mod;
             }
         }
-        return dp[m - 1][n - 1] % mod;
+        return dp[n-1][m-1];
     }
 }
