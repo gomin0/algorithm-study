3d97122060f07fa5d166ae24561a55de4403398e
gomin0 <gomin0981113@naver.com>
2025-09-05 16:47:47 +0900

    [level 2] Title: 배달, Time: 2.29 ms, Memory: 9.51 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/README.md
+++ 프로그래머스/2/12978. 배달/README.md
@@ -1,10 +1,10 @@
 # [level 2] 배달 - 12978 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12978?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12978?language=python3) 
 
 ### 성능 요약
 
-메모리: 75.1 MB, 시간: 3.66 ms
+메모리: 9.51 MB, 시간: 2.29 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 12일 10:14:26
+2025년 09월 05일 16:47:42
 
 ### 문제 설명
 
--- 프로그래머스/2/12978. 배달/배달.py
+++ 프로그래머스/2/12978. 배달/배달.py
@@ -1,27 +1,37 @@
-def floyd(N, graph):
-    for k in range(1, N+1):
-        for i in range(1, N+1):
-            for j in range(1, N+1):
-                graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
-    return graph
+from collections import defaultdict
+import heapq
 
 
-def solution(N, road, K):
+def dijkstra(graph, N, start):
+    distance = {i: float('inf') for i in range(1, N+1)}
+    distance[start] = 0
+    
+    hq = []
+    heapq.heappush(hq, (0, start))
+    while hq:
+        now_dist, now_node = heapq.heappop(hq)
+        if now_dist > distance[now_node]:
+            continue
+        for next_dist, next_node in graph[now_node]:
+            new_dist = now_dist + next_dist
+            if distance[next_node] > new_dist:
+                distance[next_node] = new_dist
+                heapq.heappush(hq, (new_dist, next_node))
+                
+    return distance
+
+
+def solution(N, road, K):    
+    graph = defaultdict(list)
+    
+    for a, b, c in road:
+        graph[a].append((c, b))
+        graph[b].append((c, a))
+    
+    distance = dijkstra(graph, N, 1)
     answer = 0
-    
-    graph = [[float('inf')] * (N+1) for _ in range(N+1)]
-    
     for i in range(1, N+1):
-        graph[i][i] = 0
-    
-    for start, end, time in road:
-        graph[start][end] = min(graph[start][end], time)
-        graph[end][start] = min(graph[end][start], time)
-    
-    graph = floyd(N, graph)
-    
-    for i in range(1, N+1):
-        if graph[1][i] <= K:
+        if distance[i] <= K:
             answer += 1
-    
+
     return answer
