2a7e0b7ad453765b5452fabda12fff6a425014dc
gomin0 <gomin0981113@naver.com>
2025-09-05 11:25:10 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 292.83 ms, Memory: 50.6 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/README.md
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/README.md
@@ -1,10 +1,10 @@
 # [level 2] 연속된 부분 수열의 합 - 178870 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/178870?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/178870?language=python3) 
 
 ### 성능 요약
 
-메모리: 149 MB, 시간: 95.05 ms
+메모리: 50.6 MB, 시간: 292.83 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 31일 13:59:32
+2025년 09월 05일 11:25:05
 
 ### 문제 설명
 
--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
@@ -1,35 +1,18 @@
 def solution(sequence, k):
+    length = float('inf')
+    n = len(sequence)
+    left = 0
+    right = 0
+    _sum = 0
     answer = []
+    while right < n:
+        _sum += sequence[right]
+        while _sum >= k:
+            if _sum == k and length > right - left:
+                length = right - left
+                answer = [left, right]
+            _sum -= sequence[left]
+            left += 1
+        right += 1
     
-    sum_list = []
-    
-    # for i in range(len(sequence)):
-    #     sum_num = 0
-    #     for j in range(i, len(sequence)):
-    #         sum_num += sequence[j]
-    #         if sum_num == k:
-    #             sum_list.append((i, j))
-    #             break
-    start, end = 0, 0
-    sum_num = 0
-    while end < len(sequence):
-        sum_num += sequence[end]
-        while sum_num >= k:
-            if sum_num == k:
-                sum_list.append((start, end))
-            
-            sum_num -= sequence[start]
-            start += 1
-        
-        end += 1
-    
-    a, b = sum_list[0]
-    length = b-a
-    if len(sum_list) > 1:
-        for k in range(1, len(sum_list)):
-            c, d = sum_list[k]
-            if length > d - c:
-                a, b = c, d
-                length = d - c
-    
-    return [a, b]
+    return answer
