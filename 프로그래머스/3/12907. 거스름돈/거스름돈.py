38bc297dc92330cc57be8316dc1ff2291e57d51a
gomin0 <gomin0981113@naver.com>
2025-09-08 14:24:34 +0900

    [level 3] Title: 거스름돈, Time: 491.94 ms, Memory: 12.2 MB -BaekjoonHub

--- 프로그래머스/3/12907. 거스름돈/거스름돈.py
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.py
@@ -1,11 +1,10 @@
-def solution(n, money) -> int:
-    answer: int = 0
+def solution(n, money):
     
-    dp: list[int] = [0 for _ in range(n+1)]
+    dp = [0] * (n+1)
     dp[0] = 1
-    
     for m in money:
         for i in range(m, n+1):
-            dp[i] += dp[i-m]
+            dp[i] += dp[i-m] % 1000000007
+
     
     return dp[n]
