67e0af9c35acba05b67a307a37514c410aeb1df9
gomin0 <gomin0981113@naver.com>
2025-09-08 17:02:54 +0900

    [level 3] Title: 입국심사, Time: 57.67 ms, Memory: 97.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -0,0 +1,32 @@
+class Solution {
+    public long solution(int n, int[] times) {
+        long minTime = Long.MAX_VALUE;
+        for (long time : times) {
+            if (time < minTime) minTime = time;
+        }
+        
+        long left = 0L;
+        long right = (long) minTime * n;
+        long answer = 0;
+        while (left <= right) {
+            long mid = (long) (left + right) / 2;
+            
+            if (can(mid, times, n)) {
+                answer = mid;
+                right = mid - 1;
+            } else left = mid + 1;
+            
+        }
+        
+        return answer;
+    }
+    
+    private boolean can(long mid, int[] times, long n) {
+        long done = 0L;
+        for (int time : times) {
+            done += mid / time;
+            if (done >= n) return true;
+        }
+        return false;
+    }
+}
