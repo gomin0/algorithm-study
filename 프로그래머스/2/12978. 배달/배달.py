578c9baad791c7765e3e10165749f5555757f943
gomin0 <gomin0981113@naver.com>
2025-09-26 21:35:11 +0900

    [level 2] Title: 배달, Time: 1.46 ms, Memory: 9.67 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.py
+++ 프로그래머스/2/12978. 배달/배달.py
@@ -1,24 +1,36 @@
-def floyd(N, graph):
-    for k in range(1, N+1):
-        for i in range(1, N+1):
-            for j in range(1, N+1):
-                graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
-    return graph
+from collections import defaultdict
+import heapq
+
+
+def dijkstra(N, graph, start):
+    distance = {i: float('inf') for i in range(1, N+1)}
+    distance[start] = 0
+    hq = []
+    heapq.heappush(hq, (0, start))
+    
+    while hq:
+        now_dist, now_node = heapq.heappop(hq)
+        if now_dist > distance[now_node]:
+            continue
+        for next_dist, next_node in graph[now_node]:
+            new_dist = now_dist + next_dist
+            if distance[next_node] > new_dist:
+                distance[next_node] = new_dist
+                heapq.heappush(hq, (new_dist, next_node))
+    
+    return distance
 
 
 def solution(N, road, K):
-    answer = 0
-    graph = [[float('inf')] * (N+1) for _ in range(N+1)]
-    for i in range(1, N+1):
-        graph[i][i] = 0
+    graph = defaultdict(list)
     for a, b, c in road:
-        graph[a][b] = min(graph[a][b], c)
-        graph[b][a] = min(graph[b][a], c)
-    graph = floyd(N, graph)
-    
+        graph[a].append((c, b))
+        graph[b].append((c, a))
+
+    distance = dijkstra(N, graph, 1)
     answer = 0
     for i in range(1, N+1):
-        if graph[1][i] <= K:
+        if distance[i] <= K:
             answer += 1
 
     return answer
