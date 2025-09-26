e9c06603bc381c4d9a8e6230db772957fca8512c
gomin0 <gomin0981113@naver.com>
2025-09-26 21:56:38 +0900

    [level 3] Title: 네트워크, Time: 0.77 ms, Memory: 9.47 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,4 +1,4 @@
-from collections import defaultdict
+from collections import defaultdict, deque
 import heapq
 
 
@@ -11,7 +11,7 @@ def solution(n, computers):
                 graph[i].append(j)
                 graph[j].append(i)
     
-    def dfs(start, graph):
+    def dfs(start):
         stack = []
         stack.append(start)
         visited.add(start)
@@ -22,12 +22,22 @@ def solution(n, computers):
                     visited.add(next_node)
                     stack.append(next_node)
             
+    def bfs(start):
+        q = deque()
+        q.append(start)
+        visited.add(start)
+        while q:
+            node = q.popleft()
+            for next_node in graph[node]:
+                if next_node not in visited:
+                    visited.add(next_node)
+                    q.append(next_node)
         
-    # def bfs(node, graph):
     answer = 0
     for i in range(n):
         if i not in visited:
-            dfs(i, graph)
+            # dfs(i)
+            bfs(i)
             answer += 1
     
     return answer
