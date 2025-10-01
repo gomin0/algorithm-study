69469e2eacddbc0bd850f491981a2490a4c2d83b
gomin0 <gomin0981113@naver.com>
2025-10-01 19:45:55 +0900

    [level 4] Title: 주문량이 많은 아이스크림들 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
+++ 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
@@ -1,10 +1,10 @@
 select f.flavor
-from first_half as f
+from first_half f
 join (
     select flavor, sum(total_order) as july_total_order
     from july as j
     group by flavor
-) as j
+) j
 on f.flavor = j.flavor
 order by (f.total_order + j.july_total_order) desc
 limit 3;
