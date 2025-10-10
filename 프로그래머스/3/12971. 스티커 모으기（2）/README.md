65c6f84698161e5f36b3d1ef4a3ae63064a9b066
gomin0 <gomin0981113@naver.com>
2025-10-10 10:37:45 +0900

    [level 3] Title: 스티커 모으기(2), Time: 63.61 ms, Memory: 15.6 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/README.md
+++ 프로그래머스/3/12971. 스티커 모으기（2）/README.md
@@ -1,10 +1,10 @@
 # [level 3] 스티커 모으기(2) - 12971 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12971?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12971?language=python3) 
 
 ### 성능 요약
 
-메모리: 74.1 MB, 시간: 0.03 ms
+메모리: 15.6 MB, 시간: 63.61 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 25일 13:17:56
+2025년 10월 10일 10:37:40
 
 ### 문제 설명
 
--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.py
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.py
@@ -1,20 +1,18 @@
-def solution(sticker) -> int:
-    length: int = len(sticker)
+def solution(sticker):
+    n = len(sticker)
+    if n <= 2:
+        return max(sticker)
     
-    if length == 1:
-        return sticker[0]
-    
-    dp1: list[int] = [0] * length
-    dp2: list[int] = [0] * length
+    dp1 = [0] * n
     dp1[0] = sticker[0]
     dp1[1] = dp1[0]
+    for i in range(2, n-1):
+        dp1[i] = max(dp1[i-1], dp1[i-2] + sticker[i])
+    dp1[n-1] = dp1[n-2]
+    
+    dp2 = [0] * n
     dp2[1] = sticker[1]
+    for i in range(2, n):
+        dp2[i] = max(dp2[i-1], dp2[i-2] + sticker[i])
     
-    for i in range(2, length - 1):
-        dp1[i] = max(dp1[i-2] + sticker[i], dp1[i-1])
-    
-    for i in range(2, length):
-        dp2[i] = max(dp2[i-2] + sticker[i], dp2[i-1])
-    
-    answer: int = max(dp1[-2], dp2[-1])
-    return answer
+    return max(dp1[n-1], dp2[n-1])
