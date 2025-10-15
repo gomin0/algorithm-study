863e0ab7a20e1bc5b0413527bf71235e7f5017e0
gomin0 <gomin0981113@naver.com>
2025-10-15 21:26:19 +0900

    [level 3] Title: 합승 택시 요금, Time: 27.62 ms, Memory: 66.4 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/README.md
+++ 프로그래머스/3/72413. 합승 택시 요금/README.md
@@ -1,10 +1,10 @@
 # [level 3] 합승 택시 요금 - 72413 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72413?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72413) 
 
 ### 성능 요약
 
-메모리: 71.6 MB, 시간: 131.36 ms
+메모리: 66.4 MB, 시간: 27.62 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 17일 14:41:09
+2025년 10월 15일 21:26:14
 
 ### 문제 설명
 
--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -1,36 +1,63 @@
 import java.util.*;
 
 class Solution {
+    static List<List<Node>> graph;
     static final int INF = Integer.MAX_VALUE;
     
-    public int solution(int n, int s, int a, int b, int[][] fares) {
-        int[][] dist = new int[n+1][n+1];
-        for (int i = 1; i <= n; i++) {
-            Arrays.fill(dist[i], INF);
-            dist[i][i] = 0;
+    class Node implements Comparable<Node> {
+        int n;
+        int w;
+        
+        Node(int n, int w) {
+            this.n = n;
+            this.w = w;
         }
         
+        @Override
+        public int compareTo(Node n2) {
+            return this.w - n2.w;
+        }
+        
+    }
+    public int solution(int n, int s, int a, int b, int[][] fares) {
+        graph = new ArrayList<>();
+        for (int i = 0; i <= n; i++) graph.add(new ArrayList<>());
         for (int[] fare : fares) {
             int u = fare[0], v = fare[1], w = fare[2];
-            dist[u][v] = w;
-            dist[v][u] = w;
+            graph.get(u).add(new Node(v, w));
+            graph.get(v).add(new Node(u, w));
         }
         
-        for (int k = 1; k <= n; k++) {
-            for (int i = 1; i <= n; i++) {
-                for (int j = 1; j <= n; j++) {
-                    if (dist[i][k] != INF && dist[k][j] != INF) {
-                        dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
-                    }
-                }
-            }
-        }
+        int[] dist1 = dijkstra(s, n);
+        int[] dist2 = dijkstra(a, n);
+        int[] dist3 = dijkstra(b, n);
         
         int answer = INF;
         for (int k = 1; k <= n; k++) {
-            answer = Math.min(answer, dist[s][k] + dist[k][a] + dist[k][b]);
+            answer = Math.min(answer, dist1[k] + dist2[k] + dist3[k]);
         }
         
         return answer;
+    }
+    
+    private int[] dijkstra(int start, int n) {
+        int[] dist = new int[n+1];
+        Arrays.fill(dist, INF);
+        dist[start] = 0;
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.offer(new Node(start, 0));
+        
+        while (!pq.isEmpty()) {
+            Node node = pq.poll();
+            if (node.w > dist[node.n]) continue;
+            for (Node nextNode : graph.get(node.n)) {
+                int w = nextNode.w + node.w;
+                if (w < dist[nextNode.n]) {
+                    dist[nextNode.n] = w;
+                    pq.offer(new Node(nextNode.n, w));
+                }
+            }
+        }
+        return dist;
     }
 }
